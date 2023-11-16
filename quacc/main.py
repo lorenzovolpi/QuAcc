3968f6d48c5a4e5c11638cfee22e348a4d820f25
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2023-11-16 17:14:35 +0100

    Refacotred, automatic creation of md file removed

--- quacc/main.py
+++ quacc/main.py
@@ -2,11 +2,34 @@ from traceback import print_exception as traceback
 
 import quacc.evaluation.comp as comp
 from quacc.dataset import Dataset
-from quacc.environment import env
+from quacc.environment import env, environ
+from quacc.evaluation.report import DatasetReport
 from quacc.logger import Logger
 from quacc.utils import create_dataser_dir
 
 CE = comp.CompEstimator()
+
+CREATE_MD = False
+
+
+def create_md(_env: environ, dr: DatasetReport, dataset: Dataset, log: Logger):
+    for plot_conf in _env.get_plot_confs():
+        for m in _env.METRICS:
+            output_path = _env.OUT_DIR / f"{plot_conf}_{m}.md"
+            try:
+                _repr = dr.to_md(
+                    conf=plot_conf,
+                    metric=m,
+                    estimators=CE.name[_env.PLOT_ESTIMATORS],
+                    plot_path=_env.PLOT_OUT_DIR,
+                )
+                with open(output_path, "w") as f:
+                    f.write(_repr)
+            except Exception as e:
+                log.error(
+                    f"Failed while saving configuration {plot_conf} of {dataset.name}. Exception: {e}"
+                )
+                traceback(e)
 
 
 def estimate_comparison():
@@ -24,32 +47,15 @@ def estimate_comparison():
             dr = comp.evaluate_comparison(
                 dataset,
                 estimators=CE.name[env.COMP_ESTIMATORS],
-            )
-            dr.pickle(env.OUT_DIR / f"{dataset.name}.pickle")
+            ).pickle(env.OUT_DIR / f"{dataset.name}.pickle")
         except Exception as e:
             log.error(f"Evaluation over {dataset.name} failed. Exception: {e}")
             traceback(e)
-        for plot_conf in env.get_plot_confs():
-            for m in env.METRICS:
-                output_path = env.OUT_DIR / f"{plot_conf}_{m}.md"
-                try:
-                    _repr = dr.to_md(
-                        conf=plot_conf,
-                        metric=m,
-                        estimators=CE.name[env.PLOT_ESTIMATORS],
-                        plot_path=env.PLOT_OUT_DIR,
-                    )
-                    with open(output_path, "w") as f:
-                        f.write(_repr)
-                except Exception as e:
-                    log.error(
-                        f"Failed while saving configuration {plot_conf} of {dataset.name}. Exception: {e}"
-                    )
-                    traceback(e)
-        Logger.clear_handlers()
 
-    # print(df.to_latex(float_format="{:.4f}".format))
-    # print(utils.avg_group_report(df).to_latex(float_format="{:.4f}".format))
+        if CREATE_MD:
+            create_md(env, dr, dataset, log)
+
+        Logger.clear_handlers()
 
 
 def main():
