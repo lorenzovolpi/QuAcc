f7b566c4a4ebfd75fe190110a1c46b73cca761ba
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2023-11-22 19:19:51 +0100

    location sync added, missing plot bug fixed

--- qcpanel/util.py
+++ qcpanel/util.py
@@ -1,7 +1,6 @@
 import os
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, List
 
 import panel as pn
 
@@ -10,118 +9,112 @@ from quacc.evaluation.report import DatasetReport
 
 _plot_sizing_mode = "stretch_both"
 valid_plot_modes = defaultdict(
-    lambda: ["delta", "delta_stdev", "diagonal", "shift", "table", "shift_table"]
+    lambda: [
+        "delta_train",
+        "stdev_train",
+        "train_table",
+        "shift",
+        "shift_table",
+        "diagonal",
+    ]
 )
 valid_plot_modes["avg"] = [
     "delta_train",
     "stdev_train",
+    "train_table",
+    "shift",
+    "shift_table",
     "delta_test",
     "stdev_test",
-    "shift",
-    "train_table",
     "test_table",
-    "shift_table",
 ]
 
 
-def create_cr_plots(
+def create_plots(
     dr: DatasetReport,
     mode="delta",
     metric="acc",
     estimators=None,
-    prev=None,
+    plot_view=None,
 ):
     _prevs = [round(cr.train_prev[1] * 100) for cr in dr.crs]
-    idx = _prevs.index(prev)
-    cr = dr.crs[idx]
     estimators = CE.name[estimators]
     if mode is None:
-        mode = valid_plot_modes[str(prev)][0]
+        mode = valid_plot_modes[plot_view][0]
     _dpi = 112
-    if mode == "table":
-        return pn.pane.DataFrame(
-            cr.data(metric=metric, estimators=estimators).groupby(level=0).mean(),
-            align="center",
-        )
-    elif mode == "shift_table":
-        return pn.pane.DataFrame(
-            cr.shift_data(metric=metric, estimators=estimators).groupby(level=0).mean(),
-            align="center",
-        )
-    else:
-        return pn.pane.Matplotlib(
-            cr.get_plots(
+    match (plot_view, mode):
+        case ("avg", "train_table"):
+            _data = (
+                dr.data(metric=metric, estimators=estimators).groupby(level=1).mean()
+            )
+            return pn.pane.DataFrame(_data, align="center") if not _data.empty else None
+        case ("avg", "test_table"):
+            _data = (
+                dr.data(metric=metric, estimators=estimators).groupby(level=0).mean()
+            )
+            return pn.pane.DataFrame(_data, align="center") if not _data.empty else None
+        case ("avg", "shift_table"):
+            _data = (
+                dr.shift_data(metric=metric, estimators=estimators)
+                .groupby(level=0)
+                .mean()
+            )
+            return pn.pane.DataFrame(_data, align="center") if not _data.empty else None
+        case ("avg", _ as plot_mode):
+            _plot = dr.get_plots(
                 mode=mode,
                 metric=metric,
                 estimators=estimators,
                 conf="panel",
                 return_fig=True,
-            ),
-            tight=True,
-            format="png",
-            sizing_mode=_plot_sizing_mode,
-            # sizing_mode="scale_height",
-            # sizing_mode="scale_both",
-        )
-
-
-def create_avg_plots(
-    dr: DatasetReport,
-    mode="delta",
-    metric="acc",
-    estimators=None,
-):
-    estimators = CE.name[estimators]
-    if mode is None:
-        mode = valid_plot_modes["avg"][0]
-
-    if mode == "train_table":
-        return pn.pane.DataFrame(
-            dr.data(metric=metric, estimators=estimators).groupby(level=1).mean(),
-            align="center",
-        )
-    elif mode == "test_table":
-        return pn.pane.DataFrame(
-            dr.data(metric=metric, estimators=estimators).groupby(level=0).mean(),
-            align="center",
-        )
-    elif mode == "shift_table":
-        return pn.pane.DataFrame(
-            dr.shift_data(metric=metric, estimators=estimators).groupby(level=0).mean(),
-            align="center",
-        )
-    return pn.pane.Matplotlib(
-        dr.get_plots(
-            mode=mode,
-            metric=metric,
-            estimators=estimators,
-            conf="panel",
-            return_fig=True,
-        ),
-        tight=True,
-        format="png",
-        # sizing_mode="scale_height",
-        sizing_mode=_plot_sizing_mode,
-        # sizing_mode="scale_both",
-    )
-
-
-def build_plot(
-    datasets: Dict[str, DatasetReport],
-    dst: str,
-    metric: str,
-    estimators: List[str],
-    view: str,
-    mode: str,
-):
-    _dr = datasets[dst]
-    if view == "avg":
-        return create_avg_plots(_dr, mode=mode, metric=metric, estimators=estimators)
-    else:
-        prev = int(view)
-        return create_cr_plots(
-            _dr, mode=mode, metric=metric, estimators=estimators, prev=prev
-        )
+            )
+            return (
+                pn.pane.Matplotlib(
+                    _plot,
+                    tight=True,
+                    format="png",
+                    # sizing_mode="scale_height",
+                    sizing_mode=_plot_sizing_mode,
+                    # sizing_mode="scale_both",
+                )
+                if _plot is not None
+                else None
+            )
+        case (_, "train_table"):
+            cr = dr.crs[_prevs.index(int(plot_view))]
+            _data = (
+                cr.data(metric=metric, estimators=estimators).groupby(level=0).mean()
+            )
+            return pn.pane.DataFrame(_data, align="center") if not _data.empty else None
+        case (_, "shift_table"):
+            cr = dr.crs[_prevs.index(int(plot_view))]
+            _data = (
+                cr.shift_data(metric=metric, estimators=estimators)
+                .groupby(level=0)
+                .mean()
+            )
+            return pn.pane.DataFrame(_data, align="center") if not _data.empty else None
+        case (_, _ as plot_mode):
+            cr = dr.crs[_prevs.index(int(plot_view))]
+            _plot = cr.get_plots(
+                mode=plot_mode,
+                metric=metric,
+                estimators=estimators,
+                conf="panel",
+                return_fig=True,
+            )
+            return (
+                pn.pane.Matplotlib(
+                    _plot,
+                    tight=True,
+                    format="png",
+                    sizing_mode=_plot_sizing_mode,
+                    # sizing_mode="scale_height",
+                    # sizing_mode="scale_both",
+                )
+                if _plot is not None
+                else None
+            )
 
 
 def explore_datasets(root: Path | str):
