cb31e26d2ef508315e21e9dd9611ba17ad876853
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2023-11-22 19:18:40 +0100

    rcalib code striped

--- baselines/rca.py
+++ baselines/rca.py
@@ -1,14 +1,5 @@
 import numpy as np
-from sklearn import clone
-from sklearn.base import BaseEstimator
 
-
-def clone_fit(c_model: BaseEstimator, data, labels):
-    c_model2 = clone(c_model)
-    c_model2.fit(data, labels)
-    return c_model2
 
 def get_score(pred1, pred2, labels):
     return np.mean((pred1 == labels).astype(int) - (pred2 == labels).astype(int))
-
-
