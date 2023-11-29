d39b2375d172c5510a422cced3e22a58c00e230e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2023-11-29 03:55:38 +0100

    plot refactored

---
+++ quacc/plot/__init__.py
@@ -0,0 +1,1 @@
+from quacc.plot.plot import get_backend, plot_delta, plot_diagonal, plot_shift
---
+++ quacc/plot/base.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+
+class BasePlot:
+    @classmethod
+    def save_fig(cls, fig, base_path, title) -> Path:
+        ...
+
+    @classmethod
+    def plot_diagonal(
+        cls,
+        reference,
+        columns,
+        data,
+        *,
+        pos_class=1,
+        title="default",
+        x_label="true",
+        y_label="estim.",
+        legend=True,
+    ):
+        ...
+
+    @classmethod
+    def plot_delta(
+        cls,
+        base_prevs,
+        columns,
+        data,
+        *,
+        stdevs=None,
+        pos_class=1,
+        title="default",
+        x_label="prevs.",
+        y_label="error",
+        legend=True,
+    ):
+        ...
+
+    @classmethod
+    def plot_shift(
+        cls,
+        shift_prevs,
+        columns,
+        data,
+        *,
+        counts=None,
+        pos_class=1,
+        title="default",
+        x_label="true",
+        y_label="estim.",
+        legend=True,
+    ):
+        ...
