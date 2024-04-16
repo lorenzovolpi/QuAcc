cc839deb7129dc212b36eeca2802f6ca86cea26a
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-04-16 15:49:21 +0200

    from_name and error constants added

--- quacc/error.py
+++ quacc/error.py
@@ -2,38 +2,34 @@ from functools import wraps
 from typing import List
 
 import numpy as np
-import quapy as qp
 from sklearn.metrics import accuracy_score, f1_score
 
 
+def from_name(err_name):
+    assert err_name in ERROR_NAMES, f"unknown error {err_name}"
+    callable_error = globals()[err_name]
+    return callable_error
+
+
 def from_contingency_table(param1, param2):
-    if (
-        param2 is None
-        and isinstance(param1, np.ndarray)
-        and param1.ndim == 2
-        and (param1.shape[0] == param1.shape[1])
-    ):
+    if param2 is None and isinstance(param1, np.ndarray) and param1.ndim == 2 and (param1.shape[0] == param1.shape[1]):
         return True
-    elif (
-        isinstance(param1, np.ndarray)
-        and isinstance(param2, np.ndarray)
-        and param1.shape == param2.shape
-    ):
+    elif isinstance(param1, np.ndarray) and isinstance(param2, np.ndarray) and param1.shape == param2.shape:
         return False
     else:
         raise ValueError("parameters for evaluation function not understood")
 
 
-def vanilla_acc_fn(param1, param2=None):
+def vanilla_acc(param1, param2=None):
     if from_contingency_table(param1, param2):
         return _vanilla_acc_from_ct(param1)
     else:
         return accuracy_score(param1, param2)
 
 
-def macrof1_fn(param1, param2=None):
+def macrof1(param1, param2=None):
     if from_contingency_table(param1, param2):
-        return macro_f1_from_ct(param1)
+        return _macro_f1_from_ct(param1)
     else:
         return f1_score(param1, param2, average="macro")
 
@@ -49,7 +45,7 @@ def _f1_bin(tp, fp, fn):
         return (2 * tp) / (2 * tp + fp + fn)
 
 
-def macro_f1_from_ct(cont_table):
+def _macro_f1_from_ct(cont_table):
     n = cont_table.shape[0]
 
     if n == 2:
@@ -83,3 +79,8 @@ def microf1(cont_table):
         fp += cont_table[:, i] - tp
         fn += cont_table[i, :] - tp
     return _f1_bin(tp, fp, fn)
+
+
+ACCURACY_MEASURE = {vanilla_acc, macrof1}
+ACCURACY_MEASURE_NAMES = {acc_fn.__name__ for acc_fn in ACCURACY_MEASURE}
+ERROR_NAMES = ACCURACY_MEASURE_NAMES
