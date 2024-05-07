8bf7ecaad11c681cff6a41a11ff4d169797a5e0e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-05-07 22:58:05 +0200

    QUACC_DATA added

--- quacc/environment.py
+++ quacc/environment.py
@@ -2,6 +2,7 @@ import os
 
 env = {
     "OUT_DIR": os.getenv("QUACC_OUT_DIR", "."),
+    "QUACC_DATA": os.getenv("QUACC_DATA", os.path.expanduser("~/quacc_data")),
     "QUAPY_DATA": os.getenv("QUACC_QUAPY_DATA", os.path.expanduser("~/quapy_data")),
     "SKLEARN_DATA": os.getenv("QUACC_SKLEARN_DATA", os.path.expanduser("~/scikit_learn_data")),
     "N_JOBS": -2,
