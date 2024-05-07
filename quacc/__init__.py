91036e165ff7696dc8ee1f18864df1240c83fdb2
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-05-07 09:45:12 +0200

    environment updated, env variables added

--- quacc/__init__.py
+++ quacc/__init__.py
@@ -1,9 +1,12 @@
-import quacc.dataset as dataset  # noqa: F401
-import quacc.error as error  # noqa: F401
-import quacc.plot as plot  # noqa: F401
-import quacc.utils.commons as commons  # noqa: F401
+# fmt: off
+import quacc.dataset as dataset
+import quacc.error as error
+import quacc.plot as plot
+import quacc.utils.commons as commons
 from quacc.environment import env
 
 
 def _get_njobs(n_jobs):
     return env["N_JOBS"] if n_jobs is None else n_jobs
+
+# fmt: on
