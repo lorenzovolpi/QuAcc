f95b217efc1d6ec8dd58399c894f75c58282b0f7
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-05-10 12:17:09 +0200

    kde added

--- playground/rcv1_multiclass_test.py
+++ playground/rcv1_multiclass_test.py
@@ -1,6 +1,8 @@
+from time import time
+
 import numpy as np
 import quapy as qp
-from quapy.method.aggregative import EMQ, PACC
+from quapy.method.aggregative import EMQ, PACC, KDEyML
 from quapy.protocol import UPP
 from sklearn.linear_model import LogisticRegression
 
@@ -16,12 +18,9 @@ qp.environ["SAMPLE_SIZE"] = 250
 NUM_TEST = 1000
 qp.environ["_R_SEED"] = 0
 
-np.seterr(all="raise")
-
-emq_lr_params = {
+pacc_lr_params = {
     "q_class__classifier__C": np.logspace(-3, 3, 7),
     "q_class__classifier__class_weight": [None, "balanced"],
-    "q_class__recalib": [None, "bcts"],
     # "add_X": [True, False],
     "add_posteriors": [True, False],
     "add_y_hat": [True, False],
@@ -29,7 +28,8 @@ emq_lr_params = {
     "add_negentropy": [True, False],
     "add_maxinfsoft": [True, False],
 }
-pacc_lr_params = {k: v for k, v in emq_lr_params.items() if k not in ["q_class__recalib"]}
+emq_lr_params = pacc_lr_params | {"q_class__recalib": [None, "bcts"]}
+kde_lr_params = pacc_lr_params | {"q_class__bandwidth": np.linspace(0.01, 0.2, 20)}
 
 if __name__ == "__main__":
     dataset_name = "C18"
@@ -45,8 +45,9 @@ if __name__ == "__main__":
         quants = [
             ("EMQ", EMQ(LogisticRegression()), emq_lr_params),
             ("PACC", PACC(LogisticRegression()), pacc_lr_params),
+            ("KDEy", KDEyML(LogisticRegression()), kde_lr_params),
         ]
-        for q_name, q, params in quants:
+        for q_name, q, params in quants[2:]:
             methods = [
                 (
                     f"QuAcc({q_name})1xn2-OPT-norefit",
@@ -56,12 +57,18 @@ if __name__ == "__main__":
                     f"QuAcc({q_name})nxn-OPT-norefit",
                     GSCAP(QuAccNxN(h, acc_fn, q), params, val_prot, acc_fn, raise_errors=True, refit=False),
                 ),
+                (
+                    "QuAcc(KDEy)1xn2-OPT",
+                    GSCAP(QuAcc1xN2(h, acc_fn, q), params, val_prot, acc_fn, raise_errors=True),
+                ),
             ]
-            for method_name, method in methods[:1]:
+            for method_name, method in methods[2:]:
+                t_init = time()
                 method.fit(V)
                 true_accs = [true_acc(h, acc_fn, Ui) for Ui in test_prot()]
                 estim_accs = [method.predict(Ui.X) for Ui in test_prot()]
                 mae = quacc.error.mae(true_accs, estim_accs)
-                results.append((method_name, acc_name, mae))
-    for method_name, acc_name, mae in results:
-        print(f"{method_name} on {acc_name}: {mae=}")
+                t_method = time() - t_init
+                results.append((method_name, acc_name, mae, t_method))
+    for method_name, acc_name, mae, t_method in results:
+        print(f"{method_name} on {acc_name}: {mae=} [took {t_method:.3f}s]")
