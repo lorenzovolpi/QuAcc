5b2c4333a28c93858e023f82615bcc59f0707a51
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-05-21 16:05:01 +0200

    N_JOBS env variable updated

--- quacc/environment.py
+++ quacc/environment.py
@@ -5,5 +5,5 @@ env = {
     "QUACC_DATA": os.getenv("QUACC_DATA", os.path.expanduser("~/quacc_data")),
     "QUAPY_DATA": os.getenv("QUACC_QUAPY_DATA", os.path.expanduser("~/quapy_data")),
     "SKLEARN_DATA": os.getenv("QUACC_SKLEARN_DATA", os.path.expanduser("~/scikit_learn_data")),
-    "N_JOBS": -2,
+    "N_JOBS": int(os.getenv("QUACC_N_JOBS", -2)),
 }
