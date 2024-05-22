7bdceb1f25cd4a77852475f3b1e49485e1b6d4e5
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-05-22 17:14:28 +0200

    fastPACC implemented

--- playground/njobs_test.py
+++ playground/njobs_test.py
@@ -1,7 +1,12 @@
+import os
+from time import time
+
 import joblib
 import numpy as np
 import quapy as qp
+from quapy.data import LabelledCollection
 from quapy.method.aggregative import PACC
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
 
 import quacc as qc
@@ -13,6 +18,39 @@ from quacc.models.model_selection import GridSearchCAP
 
 qp.environ["_R_SEED"] = 0
 qp.environ["SAMPLE_SIZE"] = 250
+
+
+class fastPACC(PACC):
+    def __init__(self, classifier: BaseEstimator, val_split=5, n_jobs=None, solver="minimize"):
+        super().__init__(classifier, val_split, n_jobs, solver)
+        self.classifier = classifier
+        self.val_split = val_split
+        self.n_jobs = qp._get_njobs(n_jobs)
+        self.solver = solver
+
+    @classmethod
+    def getPteCondEstim(cls, classes, y, y_):
+        n_classes = len(classes)
+
+        tinit = time()
+        idxs = (y == np.arange(n_classes).reshape((-1, 1))).astype(int)
+        idxs = idxs / idxs.sum(axis=-1, keepdims=True)
+
+        confusion = np.matmul(idxs, y_)
+
+        tmul = time()
+
+        _zeros_idx = np.nonzero(confusion.sum(axis=-1) == 0)[0]
+        confusion[_zeros_idx] = np.eye(n_classes)[_zeros_idx]
+
+        confusion = confusion.T
+
+        tfix = time()
+
+        print(f"{tmul - tinit:.3f}s; {tfix - tmul:.3f}s")
+
+        return confusion
+
 
 if __name__ == "__main__":
     L, V, U = DP.uci_multiclass("letter")
@@ -31,11 +69,9 @@ if __name__ == "__main__":
         "add_maxinfsoft": [True, False],
     }
 
-    print(qc.env["N_JOBS"])
-
-    joblib.parallel_config(verbose=10)
+    joblib.parallel_config(verbose=60)
     model = GridSearchCAP(
-        QuAcc1xN2(h, f1_macro, PACC(LogisticRegression())),
+        QuAcc1xN2(h, f1_macro, fastPACC(LogisticRegression())),
         pacc_lr_params,
         val_prot,
         f1_macro,
