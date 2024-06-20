aedd7da5a547ee52d7778615c7143598ec8abd6b
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-06-20 18:14:45 +0200

    Fixed diagonal grid plotting

--- quacc/experiments/plotting.py
+++ quacc/experiments/plotting.py
@@ -1,30 +1,55 @@
 import itertools as IT
 
+import pandas as pd
+
 import quacc as qc
-from quacc.experiments.generators import gen_acc_measure, gen_classifiers, get_method_names
+from quacc.experiments.generators import gen_acc_measure, gen_bin_datasets, gen_classifiers, get_method_names
 from quacc.experiments.report import Report
-from quacc.experiments.run import PROBLEM, basedir, gen_datasets, plots_basedir
+
+PROBLEM = "binary"
+basedir = PROBLEM
+plots_basedir = PROBLEM
+
+if PROBLEM == "binary":
+    gen_datasets = gen_bin_datasets
+
+
+def rename_methods(df: pd.DataFrame, methods: dict):
+    for om, nm in methods.items():
+        df.loc[df["method"] == om, "method"] = nm
 
 
 def plot_grid_of_diagonals():
-    methods = [
-        "ATC-MC",
-        "DoC",
-        "Naive",
-        "N2E(ACC-h0)",
-        "N2E(KDEy-h0)",
-    ]
+    methods = {
+        "ATC-MC": "ATC",
+        "DoC": "DoC",
+        # "Naive",
+        "N2E(ACC-h0)": "P$h$D",
+        "N2E(KDEy-h0)": "P$h$D*",
+    }
     classifiers = [
-        "KNN_10",
+        # "KNN_10",
         "LR",
-        "SVM(rbf)",
+        # "SVM(rbf)",
     ]
     for cls_name, (acc_name, _) in IT.product(classifiers, gen_acc_measure()):
         # save_plot_diagonal(basedir, cls_name, acc_name)
         dataset_names = [dataset_name for dataset_name, _ in gen_datasets(only_names=True)]
-        rep = Report.load_results(basedir, cls_name, acc_name, dataset_name=dataset_names, method_name=methods)
+        rep = Report.load_results(
+            basedir, cls_name, acc_name, dataset_name=dataset_names, method_name=list(methods.keys())
+        )
         df = rep.table_data(mean=False)
-        qc.plot.seaborn.plot_diagonal_grid(df, cls_name, acc_name, dataset_names, basedir=plots_basedir, n_cols=5)
+        rename_methods(df, methods)
+        qc.plot.seaborn.plot_diagonal_grid(
+            df,
+            cls_name,
+            acc_name,
+            dataset_names,
+            basedir=plots_basedir,
+            n_cols=5,
+            x_label="True Accuracy",
+            y_label="Estimated Accuracy",
+        )
         print(f"{cls_name}-{acc_name} plots generated")
 
 
--- quacc/plot/seaborn.py
+++ quacc/plot/seaborn.py
@@ -11,12 +11,19 @@ from quacc.utils.commons import get_plots_path
 
 sns.set_theme(style="whitegrid")
 
+DPI = 300
+
 
 def _save_figure(plot: Axes, basedir, cls_name, acc_name, dataset_name, plot_type):
+    exts = [
+        "svg",
+        "png",
+    ]
     plotsubdir = "all" if dataset_name == "*" else dataset_name
-    file = get_plots_path(basedir, cls_name, acc_name, plotsubdir, plot_type)
-    os.makedirs(Path(file).parent, exist_ok=True)
-    plot.figure.savefig(file, bbox_inches="tight")
+    files = [get_plots_path(basedir, cls_name, acc_name, plotsubdir, plot_type, ext=e) for e in exts]
+    for f in files:
+        os.makedirs(Path(f).parent, exist_ok=True)
+        plot.figure.savefig(f, bbox_inches="tight", dpi=DPI)
     plot.figure.clear()
 
 
@@ -39,9 +46,29 @@ def plot_diagonal(df: pd.DataFrame, cls_name, acc_name, dataset_name, *, basedir
     )
 
 
-def plot_diagonal_grid(df: pd.DataFrame, cls_name, acc_name, dataset_names, *, basedir=None, file_name=None, n_cols=1):
+def plot_diagonal_grid(
+    df: pd.DataFrame, cls_name, acc_name, dataset_names, *, basedir=None, file_name=None, n_cols=1, **kwargs
+):
     plot = sns.FacetGrid(df, col="dataset", col_wrap=n_cols, hue="method")
-    plot.map(sns.scatterplot, "true_accs", "estim_accs", alpha=0.5)
+    plot.map(sns.scatterplot, "true_accs", "estim_accs", alpha=0.2, s=20, edgecolor=None)
+    for ax in plot.axes.flat:
+        ax.axline((0, 0), slope=1, color="black", linestyle="--", linewidth=1)
+
+    plot.figure.subplots_adjust(hspace=0.1, wspace=0.1)
+    plot.set(xlim=(0, 1), ylim=(0, 1))
+    plot.set_titles("{col_name}")
+
+    plot.add_legend(title="")
+    sns.move_legend(plot, "lower center", bbox_to_anchor=(0.84, 0.06), ncol=1)
+    for lh in plot.legend.legend_handles:
+        lh.set_alpha(1)
+        lh.set_sizes([100])
+
+    if "x_label" in kwargs:
+        plot.set_xlabels(kwargs["x_label"])
+    if "y_label" in kwargs:
+        plot.set_ylabels(kwargs["y_label"])
+
     return _save_figure(
         plot,
         basedir,
