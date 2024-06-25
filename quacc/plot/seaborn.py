ed2642c6787ec1b17a6e16384569285de7d5892e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-06-25 22:38:13 +0200

    parameters added to grid diagonal plot

--- quacc/plot/seaborn.py
+++ quacc/plot/seaborn.py
@@ -47,27 +47,53 @@ def plot_diagonal(df: pd.DataFrame, cls_name, acc_name, dataset_name, *, basedir
 
 
 def plot_diagonal_grid(
-    df: pd.DataFrame, cls_name, acc_name, dataset_names, *, basedir=None, file_name=None, n_cols=1, **kwargs
+    df: pd.DataFrame,
+    cls_name,
+    acc_name,
+    dataset_names,
+    *,
+    basedir=None,
+    file_name=None,
+    n_cols=1,
+    x_label="true accs.",
+    y_label="estim. accs.",
+    aspect=1,
+    xticks=None,
+    yticks=None,
+    xtick_vert=False,
+    hspace=0.1,
+    wspace=0.1,
+    legend_bbox_to_anchor=(1, 0.5),
+    **kwargs,
 ):
-    plot = sns.FacetGrid(df, col="dataset", col_wrap=n_cols, hue="method")
+    plot = sns.FacetGrid(df, col="dataset", col_wrap=n_cols, hue="method", xlim=(0, 1), ylim=(0, 1), aspect=aspect)
     plot.map(sns.scatterplot, "true_accs", "estim_accs", alpha=0.2, s=20, edgecolor=None)
     for ax in plot.axes.flat:
         ax.axline((0, 0), slope=1, color="black", linestyle="--", linewidth=1)
+        if xtick_vert:
+            ax.tick_params(axis="x", labelrotation=90, labelsize=10)
+            ax.tick_params(axis="y", labelsize=10)
+        if xticks is not None:
+            ax.set_xticks(xticks)
+        if yticks is not None:
+            ax.set_yticks(yticks)
 
-    plot.figure.subplots_adjust(hspace=0.1, wspace=0.1)
-    plot.set(xlim=(0, 1), ylim=(0, 1))
+    plot.figure.subplots_adjust(hspace=hspace, wspace=wspace)
     plot.set_titles("{col_name}")
 
     plot.add_legend(title="")
-    sns.move_legend(plot, "lower center", bbox_to_anchor=(0.84, 0.06), ncol=1)
+    sns.move_legend(
+        plot,
+        "lower center",
+        bbox_to_anchor=legend_bbox_to_anchor,
+        ncol=kwargs.get("legend_ncol", 1),
+    )
     for lh in plot.legend.legend_handles:
         lh.set_alpha(1)
         lh.set_sizes([100])
 
-    if "x_label" in kwargs:
-        plot.set_xlabels(kwargs["x_label"])
-    if "y_label" in kwargs:
-        plot.set_ylabels(kwargs["y_label"])
+    plot.set_xlabels(x_label)
+    plot.set_ylabels(y_label)
 
     return _save_figure(
         plot,
