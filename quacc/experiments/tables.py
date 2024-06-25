0ba1347f3f1e876dbf28bb05c0c078c0051de05d
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-06-25 22:36:03 +0200

    tables refactored, hstack table extracted, parameters moved to globals

--- quacc/experiments/tables.py
+++ quacc/experiments/tables.py
@@ -10,7 +10,7 @@ import pandas as pd
 import quacc as qc
 from quacc.experiments.generators import gen_acc_measure, gen_bin_datasets, gen_classifiers
 from quacc.experiments.report import Report
-from quacc.utils.table import Format, Table
+from quacc.table import Format, Table
 
 PROBLEM = "binary"
 ERROR = qc.error.ae
@@ -18,10 +18,31 @@ ERROR = qc.error.ae
 if PROBLEM == "binary":
     gen_datasets = gen_bin_datasets
 
+BENCHMARKS = [name for name, _ in gen_datasets(only_names=True)]
+METHODS = ["Naive", "ATC-MC", "DoC", "N2E(ACC-h0)", "N2E(KDEy-h0)"]
+CLASSIFIERS = ["LR", "KNN_10", "SVM(rbf)", "MLP"]
+ACC_NAMES = [acc_name for acc_name, _ in gen_acc_measure()]
+
+
+def rename_method(m):
+    methods_dict = {
+        "ATC-MC": "ATC",
+        "Naive": "\\naive",
+        "N2E(ACC-h0)": "\\phd",
+        "N2E(KDEy-h0)": "\\phdplus",
+    }
+    return methods_dict.get(m, m)
+
+
+def rename_cls(cls):
+    cls_dict = {"SVM(rbf)": "SVM", "KNN_10": "$k$-NN"}
+    return cls_dict.get(cls, cls)
+
 
 def table_from_df(df: pd.DataFrame, name, benchmarks, methods) -> Table:
     tbl = Table(name=name, benchmarks=benchmarks, methods=methods)
-    tbl.format = Format(mean_prec=4, show_std=False, remove_zero=True, with_rank_mean=False)
+    tbl.format = Format(mean_prec=3, show_std=False, remove_zero=True, with_rank_mean=False)
+    tbl.format.mean_macro = False
     for dataset, method in IT.product(benchmarks, methods):
         values = df.loc[(df["dataset"] == dataset) & (df["method"] == method), ["acc_err"]].to_numpy()
         for v in values:
@@ -34,46 +55,7 @@ def get_method_cls_name(method, cls):
     return f"{method}-{cls}"
 
 
-def gen_n2e_tables():
-    pdf_path = f"tables/n2e/{PROBLEM}.pdf"
-
-    def rename_method(m):
-        methods_dict = {
-            "Naive": "\\naive",
-            "N2E(ACC-h0)": "\\phd",
-            "N2E(KDEy-h0)": "\\phdplus",
-        }
-        return methods_dict.get(m, m)
-
-    def rename_cls(cls):
-        cls_dict = {"SVM(rbf)": "SVM", "KNN_10": "$k$-NN"}
-        return cls_dict.get(cls, cls)
-
-    benchmarks = [name for name, _ in gen_datasets(only_names=True)]
-    base_methods = ["Naive", "ATC-MC", "DoC", "N2E(ACC-h0)", "N2E(KDEy-h0)"]
-    tbl_methods = [rename_method(m) for m in base_methods]
-    # base_cls = [cls_name for cls_name, _ in gen_classifiers()]
-    base_cls = ["LR", "KNN_10", "SVM(rbf)"]
-    acc_names = [acc_name for acc_name, _ in gen_acc_measure()]
-
-    tables = []
-    for acc_name in acc_names:
-        for cls_name in base_cls:
-            rep = Report.load_results(PROBLEM, cls_name, acc_name, benchmarks, base_methods)
-            df = rep.table_data(mean=False, error=ERROR)
-
-            # rename methods and cls
-            cls_name = rename_cls(cls_name)
-            # for m in base_methods:
-            #     df.loc[df["method"] == m, ["method"]] = rename_method(m)
-
-            # build table
-            tbl_name = f"{PROBLEM}_{cls_name}_{acc_name}"
-            tbl = table_from_df(df, name=tbl_name, benchmarks=benchmarks, methods=base_methods)
-            tables.append(tbl)
-
-    Table.LatexPDF(pdf_path=pdf_path, tables=tables, landscape=False)
-
+def hstack_tables(tables, pdf_path):
     tbl_dict = defaultdict(lambda: [])
     tbl_endline = defaultdict(lambda: "\\\\")
     for t in tables:
@@ -105,23 +87,47 @@ def gen_n2e_tables():
         row = " & ".join(row_l)
         corpus.append(f"{name} & {row} {tbl_endline[name]}")
     corpus = "\n".join(corpus) + "\n"
-    begin = "\\begin{tabular}{|c|" + (("c" * len(base_methods)) + "|") * len(base_cls) + "}\n"
+    header = "\\setlength{\\tabcolsep}{0pt}\n"
+    begin = "\\begin{tabular}{|c|" + (("c" * len(METHODS)) + "|") * len(CLASSIFIERS) + "}\n"
     end = "\\end{tabular}\n"
-    cline = "\\cline{2-" + str(len(base_methods) * len(base_cls) + 1) + "}\n"
+    cline = "\\cline{2-" + str(len(METHODS) * len(CLASSIFIERS) + 1) + "}\n"
     multicol1 = (
         "\\multicolumn{1}{c|}{} & "
-        + " & ".join(["\\multicolumn{" + str(len(base_methods)) + "}{c|}{" + rename_cls(cls) + "}" for cls in base_cls])
+        + " & ".join(["\\multicolumn{" + str(len(METHODS)) + "}{c|}{" + rename_cls(cls) + "}" for cls in CLASSIFIERS])
         + " \\\\\n"
     )
+    tbl_methods = [rename_method(m) for m in METHODS]
     multicol2 = (
         "\\multicolumn{1}{c|}{} & "
-        + " & ".join([" & ".join([m for m in tbl_methods]) for _ in base_cls])
+        + " & ".join([" & ".join(["\\side{" + m + "}" for m in tbl_methods]) for _ in CLASSIFIERS])
         + " \\\\\\hline\n"
     )
 
-    hstack_path = os.path.join(str(Path(pdf_path).parent), f"{PROBLEM}_hstack.tex")
+    hstack_dir = os.path.join(str(Path(pdf_path).parent), "tables")
+    os.makedirs(hstack_dir, exist_ok=True)
+    hstack_path = os.path.join(hstack_dir, f"{PROBLEM}_hstack.tex")
     with open(hstack_path, "w") as f:
-        f.write(begin + cline + multicol1 + cline + multicol2 + corpus + end)
+        f.write(header + begin + cline + multicol1 + cline + multicol2 + corpus + end)
+
+
+def gen_n2e_tables():
+    pdf_path = f"tables/n2e/{PROBLEM}.pdf"
+
+    tables = []
+    for acc_name in ACC_NAMES:
+        for cls_name in CLASSIFIERS:
+            rep = Report.load_results(PROBLEM, cls_name, acc_name, BENCHMARKS, METHODS)
+            df = rep.table_data(mean=False, error=ERROR)
+
+            cls_name = rename_cls(cls_name)
+
+            # build table
+            tbl_name = f"{PROBLEM}_{cls_name}_{acc_name}"
+            tbl = table_from_df(df, name=tbl_name, benchmarks=BENCHMARKS, methods=METHODS)
+            tables.append(tbl)
+
+    Table.LatexPDF(pdf_path=pdf_path, tables=tables, landscape=False)
+    hstack_tables(tables, pdf_path)
 
 
 if __name__ == "__main__":
