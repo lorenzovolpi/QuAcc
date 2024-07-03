4722d08e9861f1e6bbea483bd055224dea4d5b26
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-07-03 16:42:54 +0200

    formatting, changed default server

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
-SERVER="dgx"
+SERVER="bcuda"
 while [[ $# -gt 0 ]]; do
-    case $1 in
-        -h|--host)
-            SERVER="$2"
-            shift
-            shift
-            ;;
-        *)
-            echo "Unknown option $1"
-            exit 1
-            ;;
-    esac
+  case $1 in
+  -h | --host)
+    SERVER="$2"
+    shift
+    shift
+    ;;
+  *)
+    echo "Unknown option $1"
+    exit 1
+    ;;
+  esac
 done
 
 if [[ $SERVER == "dgx" ]]; then
-    HOST="lorenzovolpi@edge-nd1.isti.cnr.it"
-    WORKDIR="raid/quacc"
+  HOST="lorenzovolpi@edge-nd1.isti.cnr.it"
+  WORKDIR="raid/quacc"
 
-    IMAGE_NAME="lorenzovolpi/quacc_main"
-    DEST="$HOST:$WORKDIR"
+  IMAGE_NAME="lorenzovolpi/quacc_main"
+  DEST="$HOST:$WORKDIR"
 
-    rsync -ai quacc playground $DEST
-    poetry export --without-hashes --with=dev,dash --format=requirements.txt 2>/dev/null 1>requirements.txt
-    rsync -i build/Dockerfile requirements.txt build/drun.sh build/ddebug.sh build/dbuild.sh build/docker.env build/.dockerignore $DEST
-    ssh $HOST "cd $WORKDIR; ./dbuild.sh $IMAGE_NAME"
+  rsync -ai quacc playground $DEST
+  poetry export --without-hashes --with=dev,dash --format=requirements.txt 2>/dev/null 1>requirements.txt
+  rsync -i build/Dockerfile requirements.txt build/drun.sh build/ddebug.sh build/dbuild.sh build/docker.env build/.dockerignore $DEST
+  ssh $HOST "cd $WORKDIR; ./dbuild.sh $IMAGE_NAME"
 elif [[ $SERVER == "bcuda" || $SERVER == "barracuda" ]]; then
-    HOST="volpi@barracuda.isti.cnr.it"
-    WORKDIR="quacc"
+  HOST="volpi@barracuda.isti.cnr.it"
+  WORKDIR="quacc"
 
-    DEST="$HOST:$WORKDIR"
+  DEST="$HOST:$WORKDIR"
 
-    rsync -ai quacc playground $DEST
-    rsync -i README.md build/run.sh build/bcuda.env pyproject.toml $DEST
+  rsync -ai quacc playground $DEST
+  rsync -i README.md build/run.sh build/bcuda.env pyproject.toml $DEST
 fi
-
-
-
