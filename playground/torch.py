584ac9089ce8c0ae879ec5475c18e38659bd6f6b
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-09-19 13:56:05 +0200

    distilbert model and collection classes refactored

--- playground/torch.py
+++ playground/torch.py
@@ -1,5 +1,3 @@
-import contextlib
-import os
 from collections import defaultdict
 from typing import Callable, List
 
@@ -7,217 +5,30 @@ import datasets
 import numpy as np
 import pandas as pd
 import quapy as qp
-import torch
-import torch.nn as nn
-import torch.utils.data
 from datasets import load_dataset
-from quapy.data.base import LabelledCollection
 from quapy.method.aggregative import SLD
 from quapy.protocol import UPP
-from scipy.sparse import issparse
 from sklearn.kernel_ridge import KernelRidge as KRR
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier as MLP
-from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import (
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    DataCollatorWithPadding,
-    DistilBertForSequenceClassification,
-    get_scheduler,
-)
 
-import quacc as qc
+from quacc.data.base import TorchLabelledCollection
 from quacc.error import vanilla_acc
 from quacc.experiments.util import split_validation
+from quacc.models._large_models import DistilBert
 from quacc.models.cont_table import QuAcc1xN2, QuAcc1xNp1, QuAccNxN
 from quacc.models.direct import DoC
 from quacc.models.model_selection import GridSearchCAP
 from quacc.models.regression import ReQua
-from quacc.models.utils import get_posteriors_from_h
 
 NUM_SAMPLES = 100
 SAMPLE_SIZE = 500
-RANDOM_STATE = 313
+RANDOM_STATE = 42
 
 qp.environ["_R_SEED"] = RANDOM_STATE
 qp.environ["SAMPLE_SIZE"] = SAMPLE_SIZE
-
-
-def softmax(logits: torch.Tensor) -> np.ndarray:
-    return nn.functional.softmax(logits, dim=-1).numpy()
-
-
-class TorchLabelledCollection(LabelledCollection):
-    def __init__(self, instances, labels, attention_mask, classes=None):
-        self.attention_mask = attention_mask
-        super().__init__(instances, labels, classes)
-
-    def sampling_from_index(self, index):
-        documents = self.instances[index]
-        labels = self.labels[index]
-        attention_mask = self.attention_mask[index]
-        return TorchLabelledCollection(documents, labels, attention_mask, classes=self.classes_)
-
-
-class TorchDataset(torch.utils.data.Dataset):
-    DATA_MAPPING = {
-        "X": "input_ids",
-        "y": "labels",
-        "mask": "attention_mask",
-    }
-
-    def __init__(self, X, y, attention_mask, data_mapping=None, has_labels=True):
-        self.X = X
-        self.y = y
-        self.attention_mask = attention_mask
-        self.data_mapping = self.DATA_MAPPING if data_mapping is None else data_mapping
-        self.has_labels = has_labels
-
-    def __len__(self):
-        return self.X.shape[0]
-
-    def __getitem__(self, index):
-        res = {}
-        if issparse(self.X):
-            res["X"] = self.X[index, :].toarray()
-        else:
-            res["X"] = self.X[index, :]
-
-        if self.has_labels:
-            res["y"] = self.y[index]
-
-        res["mask"] = self.attention_mask[index, :]
-
-        return {self.data_mapping[k]: v for k, v in res.items()}
-
-    @classmethod
-    def from_lc(cls, data: TorchLabelledCollection, data_mapping=None):
-        return TorchDataset(*data.Xy, data.attention_mask, data_mapping=data_mapping)
-
-    @classmethod
-    def from_X(cls, data, attention_mask, data_mapping=None):
-        return TorchDataset(data, None, attention_mask, data_mapping=data_mapping, has_labels=False)
-
-
-class LargeModel:
-    def fit(self, train: LabelledCollection, dataset_name: str): ...
-
-    def predict_proba(self, test) -> np.ndarray: ...
-
-    def get_model_path(self, dataset_name):
-        return os.path.join(qc.env["OUT_DIR"], "models", f"{self.name}_on_{dataset_name}")
-
-    def predict_from_proba(self, proba: np.ndarray) -> np.ndarray:
-        return np.argmax(proba, axis=-1)
-
-    def predict(self, test) -> np.ndarray:
-        return self.predict_from_proba(self.predict_proba(test))
-
-
-class DistilBert(LargeModel):
-    def __init__(self, learning_rate=1e-5, batch_size=64, epochs=3, seed=RANDOM_STATE):
-        self.name = "distilbert-base-uncased"
-        self.tokenizer_name = self.name
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
-        self.num_epochs = epochs
-        self.seed = seed
-        self.epoch = 0
-
-        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
-        self.data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer, padding="max_length")
-        self.model: DistilBertForSequenceClassification = AutoModelForSequenceClassification.from_pretrained(self.name)
-
-    def get_model_path(self, dataset_name):
-        return os.path.join(qc.env["OUT_DIR"], "models", f"{self.name}_on_{dataset_name}.tar")
-
-    def checkpoint(self, dataset_name):
-        path = self.get_model_path(dataset_name)
-        torch.save(
-            {
-                "epoch": self.epoch,
-                "model_state_dict": self.model.state_dict(),
-                "optimizer_state_dict": self.optimizer.state_dict(),
-            },
-            path,
-        )
-
-    def load_from_checkpoint(self, dataset_name):
-        path = self.get_model_path(dataset_name)
-        if os.path.exists(path):
-            _checkpoint = torch.load(path)
-            self.model.load_state_dict(_checkpoint["model_state_dict"])
-            self.optimizer.load_state_dict(_checkpoint["optimizer_state_dict"])
-            self.epoch = _checkpoint["epoch"]
-
-    def fit(self, train: TorchLabelledCollection, dataset_name: str, verbose=True):
-        self.classes_ = train.classes_
-        train_dl = DataLoader(
-            TorchDataset.from_lc(train),
-            batch_size=self.batch_size,
-        )
-        self.optimizer = AdamW(self.model.parameters(), lr=self.learning_rate)
-        num_training_steps = self.num_epochs * len(train_dl)
-
-        self.load_from_checkpoint(dataset_name)
-
-        if self.num_epochs == self.epoch:
-            return self
-
-        lr_scheduler = get_scheduler(
-            name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-        )
-
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        self.model.to(device)
-
-        if verbose:
-            progress_bar = tqdm(range(num_training_steps))
-
-        self.model.train()
-        for epoch in range(self.num_epochs - self.epoch):
-            self.epoch = epoch + 1
-            for batch in train_dl:
-                batch = {k: v.to(device) for k, v in batch.items()}
-                outputs = self.model(**batch)
-                loss = outputs.loss
-                loss.backward()
-
-                self.optimizer.step()
-                lr_scheduler.step()
-                self.optimizer.zero_grad()
-                if verbose:
-                    progress_bar.update(1)
-
-        self.checkpoint(dataset_name)
-
-        return self
-
-    def predict_proba(self, test, attention_mask, verbose=False) -> np.ndarray:
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-        test_dl = DataLoader(
-            TorchDataset.from_X(test, attention_mask),
-            batch_size=self.batch_size,
-        )
-        self.model.to(device)
-
-        self.model.eval()
-        y_probs = []
-        if verbose:
-            progress_bar = tqdm(range(len(test_dl)))
-        with torch.no_grad():
-            for batch in test_dl:
-                batch = {k: v.to(device) for k, v in batch.items()}
-                outputs = self.model(**batch)
-                y_probs.append(softmax(outputs.logits.cpu()))
-                if verbose:
-                    progress_bar.update(1)
-
-        return np.vstack(y_probs)
 
 
 def preprocess_data(
@@ -255,16 +66,16 @@ def sld():
 
 
 def main():
-    BASE = False
-    OPT_N2 = False
-    OPT_NN = False
+    BASE = True
+    OPT_N2 = True
+    OPT_NN = True
     REQUA = True
 
     model = DistilBert()
 
     dataset_map = {
-        # "imdb": (["text"], 25000),
-        # "rotten_tomatoes": (["text"], 8530),
+        "imdb": (["text"], 25000),
+        "rotten_tomatoes": (["text"], 8530),
         "amazon_polarity": (["title", "content"], 25000),
     }
 
@@ -358,9 +169,6 @@ def main():
                 n_jobs=0,
             ).fit(V1, V1_posteriors)
             print("requa fit")
-            print(f"V1 shape: {V1.X.shape}")
-            for i, m in enumerate(requa.models):
-                print(f"V1 #{i} re-shape: {m._get_X_dot(V1.X, V1_posteriors).shape}")
 
         doc = DoC(vanilla_acc, V2_prot, V2_prot_posteriors).fit(V1, V1_posteriors)
         print("doc fit")
