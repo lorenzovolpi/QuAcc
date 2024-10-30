2b092ac1f4cfd72b17ca2d59d91145b4742864cf
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-10-30 22:34:03 +0100

    improved test

--- quacc/experiments/ctdiff.py
+++ quacc/experiments/ctdiff.py
@@ -1,8 +1,19 @@
 import numpy as np
+from quapy.method.aggregative import EMQ, KDEyML
 from quapy.protocol import UPP
+from sklearn.linear_model import LogisticRegression as LR
 
+from quacc.error import vanilla_acc
 from quacc.experiments.util import split_validation
-from quacc.models.cont_table import CAPContingencyTable
+from quacc.models.cont_table import LEAP, PHD, CAPContingencyTable
+
+
+def sld():
+    return EMQ(LR(), val_split=5)
+
+
+def kdey():
+    return KDEyML(LR())
 
 
 class PredictedSet:
@@ -20,7 +31,11 @@ def gen_datasets():
 
 
 def gen_methods(h, V_ps, V1_ps, V2_prot_ps):
-    pass
+    acc_fn = vanilla_acc
+    yield "LEAP(SLD)", LEAP(acc_fn, sld(), reuse_h=h), V_ps
+    yield "PHD(SLD)", PHD(acc_fn, sld(), reuse_h=h)
+    yield "LEAP(KDEy)", LEAP(acc_fn, kdey(), reuse_h=h)
+    yield "PHD(KDEy)", PHD(acc_fn, kdey(), reuse_h=h)
 
 
 def gen_classifier_dataset():
@@ -30,7 +45,10 @@ def gen_classifier_dataset():
 
 
 def get_cts(method: CAPContingencyTable, test_prot, test_prot_posteriors):
-    pass
+    cts_list = []
+    for sample, sample_post in zip(test_prot(), test_prot_posteriors):
+        cts_list.appen(method.predict_ct(sample, sample_post))
+    return np.asarray(cts_list)
 
 
 def ctdiff():
@@ -56,9 +74,14 @@ def ctdiff():
             test_prot_posteriors.append(P)
             test_prot_y_hat.append(np.argmax(P, axis=-1))
 
+        out_map = {}
         for method_name, method, val_ps in gen_methods(h, V_ps, V1_ps, V2_prot_ps):
             val, val_posteriors = val_ps.A, val_ps.post
             method.fit(val, val_posteriors)
+            estim_cts = get_cts(method, test_prot, test_prot_posteriors)
+            out_map[method_name] = estim_cts
+
+        print(out_map)
 
 
 if __name__ == "__main__":
