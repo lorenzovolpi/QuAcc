dbe9d34a2238e182b82ee7863b58dbef83cf1a28
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-11-29 16:00:29 +0100

    improved requa test

--- playground/requa_test.py
+++ playground/requa_test.py
@@ -1,6 +1,5 @@
 import os
 from contextlib import redirect_stdout
-from glob import glob
 from time import time
 
 import numpy as np
@@ -9,8 +8,8 @@ import quapy as qp
 from quapy.method.aggregative import EMQ, KDEyML
 from quapy.protocol import UPP
 from sklearn.kernel_ridge import KernelRidge as KRR
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression as LinReg
+from sklearn.linear_model import LogisticRegression, Ridge
 
 import quacc as qc
 import quacc.error
@@ -18,7 +17,7 @@ from quacc.data.datasets import RCV1_MULTICLASS_DATASETS, fetch_RCV1BinaryDatase
 from quacc.error import vanilla_acc
 from quacc.experiments.util import split_validation
 from quacc.models.cont_table import QuAcc1xN2, QuAcc1xNp1, QuAccNxN
-from quacc.models.regression import reDAN
+from quacc.models.regression import ReQua
 from quacc.utils.commons import true_acc
 
 NUM_TEST = 1000
@@ -26,7 +25,6 @@ qp.environ["_R_SEED"] = 0
 
 
 CSV_SEP = ","
-LOCAL_DIR = os.path.join(qc.env["OUT_DIR"], "pg_results", "requa")
 CONFIG = "binary"
 VERBOSE = True
 
@@ -72,6 +70,10 @@ def gen_multi_datasets():
         yield dataset_name, fetch_RCV1MulticlassDataset(dataset_name)
 
 
+def gen_classifiers():
+    yield "LR", LogisticRegression()
+
+
 if CONFIG == "multiclass":
     get_quaccs = get_multi_quaccs
     gen_datasets = gen_multi_datasets
@@ -87,90 +89,89 @@ elif CONFIG == "binary":
 # fmt: off
 
 def gen_methods(h, acc_fn):
-    # quacc_params = {
-    #     "q_class__classifier__C": np.logspace(-3, 3, 7),
-    #     "q_class__classifier__class_weight": [None, "balanced"],
-    #     "add_X": [True, False],
-    #     "add_posteriors": [True, False],
-    #     "add_y_hat": [True, False],
-    #     "add_maxconf": [True, False],
-    #     "add_negentropy": [True, False],
-    #     "add_maxinfsoft": [True, False],
-    # }
+    quacc_params = {
+        # "q_class__classifier__C": np.logspace(-3, 3, 7),
+        # "q_class__classifier__class_weight": [None, "balanced"],
+        "add_X": [True, False],
+        "add_posteriors": [True, False],
+        "add_y_hat": [True, False],
+        "add_maxconf": [True, False],
+        "add_negentropy": [True, False],
+        "add_maxinfsoft": [True, False],
+    }
     sample_s = qp.environ["SAMPLE_SIZE"]
-    # yield "ReQua(SLD-LinReg)", ReQua(h, acc_fn, LinReg(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
+    yield "ReQua(SLD-LinReg)", ReQua(h, acc_fn, LinReg(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
     # yield "ReQua(SLD-LinReg)-linfeat", ReQua(h, acc_fn, LinReg(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, add_conf=True, verbose=VERBOSE)
-    # yield "ReQua(SLD-Ridge)", ReQua(h, acc_fn, Ridge(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
+    yield "ReQua(SLD-Ridge)", ReQua(h, acc_fn, Ridge(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
     # yield "ReQua(SLD-Ridge)-linfeat", ReQua(h, acc_fn, Ridge(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, add_conf=True, verbose=VERBOSE)
-    # yield "ReQua(SLD-KRR)", ReQua(h, acc_fn, KRR(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
+    yield "ReQua(SLD-KRR)", ReQua(h, acc_fn, KRR(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
     # yield "ReQua(SLD-KRR)-linfeat", ReQua(h, acc_fn, KRR(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, add_conf=True, verbose=VERBOSE)
     # yield "ReQua(SLD-SVR)", ReQua(h, acc_fn, SVR(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, verbose=VERBOSE)
     # yield "ReQua(SLD-SVR)-linfeat", ReQua(h, acc_fn, SVR(), get_quaccs(h, acc_fn, sld()), param_grid=quacc_params, sample_size=sample_s, add_conf=True, verbose=VERBOSE)
-    yield "reDAN(SLD-KRR)-OPT", reDAN(h, acc_fn, KRR(), sld(), add_n2e_opt=True, sample_size=sample_s)
-    yield "reDAN(KDEy-KRR)-OPT", reDAN(h, acc_fn, KRR(), kdey(), add_n2e_opt=True, sample_size=sample_s)
 
 # fmt: on
 
 
-def get_local_path(dataset, method_name, acc_name):
-    return os.path.join(LOCAL_DIR, basedir, f"{dataset}_{method_name}_{acc_name}.csv")
+def gen_accs():
+    yield "vanilla_acc", vanilla_acc
+    # yield "f1", f1_macro
+
+
+def get_local_path(cls_name, acc_name, dataset, method_name):
+    parent_dir = os.path.join(qc.env["OUT_DIR"], "requa_test", basedir, cls_name, acc_name, dataset)
+    os.makedirs(parent_dir, exist_ok=True)
+    return os.path.join(parent_dir, f"{method_name}.csv")
 
 
 def main():
-    os.makedirs(LOCAL_DIR, exist_ok=True)
     dfs = []
     for dataset_name, (L, V, U) in gen_datasets():
         V, val_prot = split_validation(V)
-        # h = LogisticRegression()
-        h_param_grid = {"C": np.logspace(-4, -4, 9), "class_weight": ["balanced", None]}
-        h_name, h = "LR-OPT", GridSearchCV(LogisticRegression(), h_param_grid, cv=5, n_jobs=-1)
-        test_prot = UPP(U, repeats=NUM_TEST, return_type="labelled_collection", random_state=0)
-        accs = [
-            ("vanilla_acc", vanilla_acc),
-            # ("f1", f1_macro),
-        ]
+        for cls_name, h in gen_classifiers():
+            test_prot = UPP(U, repeats=NUM_TEST, return_type="labelled_collection", random_state=0)
 
-        h.fit(*L.Xy)
-        print(f"trained {h_name} trained over {dataset_name}")
-        for acc_name, acc_fn in accs:
-            for method_name, method in gen_methods(h, acc_fn):
-                local_path = get_local_path(dataset_name, method_name, acc_name)
+            h.fit(*L.Xy)
+            print(f"trained {cls_name} trained over {dataset_name}")
+            for acc_name, acc_fn in gen_accs():
+                for method_name, method in gen_methods(h, acc_fn):
+                    local_path = get_local_path(cls_name, acc_name, dataset_name, method_name)
 
-                if os.path.exists(local_path):
-                    method_df = pd.read_csv(local_path, sep=CSV_SEP)
+                    if os.path.exists(local_path):
+                        method_df = pd.read_csv(local_path, sep=CSV_SEP)
+                        dfs.append(method_df)
+                        print(f"method {method_name} for {acc_name} exists, skipping")
+                        continue
+
+                    t_init = time()
+                    method.fit(V)
+                    true_accs = np.array([true_acc(h, acc_fn, Ui) for Ui in test_prot()])
+                    estim_accs = method.batch_predict(test_prot)
+                    estim_accs = np.asarray(estim_accs)
+
+                    ae = quacc.error.ae(true_accs, estim_accs)
+                    t_method = time() - t_init
+                    print(f"method {method_name} for {acc_name} took {t_method:.3f}s")
+
+                    method_df = pd.DataFrame(
+                        np.vstack([true_accs, estim_accs, ae]).T,
+                        columns=["true_accs", "estim_accs", "ae"],
+                    )
+                    method_df["method"] = method_name
+                    method_df["acc_name"] = acc_name
+                    method_df["dataset"] = dataset_name
+                    method_df["cls"] = cls_name
+                    method_df.to_csv(local_path, sep=CSV_SEP)
                     dfs.append(method_df)
-                    print(f"method {method_name} for {acc_name} exists, skipping")
-                    continue
-
-                t_init = time()
-                method.fit(V)
-                true_accs = np.array([true_acc(h, acc_fn, Ui) for Ui in test_prot()])
-                estim_accs = method.batch_predict(test_prot)
-                estim_accs = np.asarray(estim_accs)
-
-                ae = quacc.error.ae(true_accs, estim_accs)
-                t_method = time() - t_init
-                print(f"method {method_name} for {acc_name} took {t_method:.3f}s")
-
-                method_df = pd.DataFrame(
-                    np.vstack([true_accs, estim_accs, ae]).T,
-                    columns=["true_accs", "estim_accs", "ae"],
-                )
-                method_df["method"] = method_name
-                method_df["acc_name"] = acc_name
-                method_df["dataset"] = dataset_name
-                method_df.to_csv(local_path, sep=CSV_SEP)
-                dfs.append(method_df)
 
     results = pd.concat(dfs)
 
     print(results.pivot_table(values="ae", index="dataset", columns=["method", "acc_name"]))
 
 
-def clean_results(path="*.csv"):
-    glob_path = os.path.join(LOCAL_DIR, basedir, path)
-    for f in glob(glob_path):
-        os.remove(f)
+# def clean_results(path="*.csv"):
+#     glob_path = os.path.join(LOCAL_DIR, basedir, path)
+#     for f in glob(glob_path):
+#         os.remove(f)
 
 
 if __name__ == "__main__":
