9b9533e24b31f8a2a6db3fb54f23bd1c6736bec3
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-12-06 16:56:31 +0100

    completed critdd experimentation

--- quacc/experiments/critdd.py
+++ quacc/experiments/critdd.py
@@ -1,9 +1,17 @@
+import itertools as IT
+import os
+import pathlib
+import subprocess
+
+from critdd import Diagram
 from quapy.data.datasets import UCI_BINARY_DATASETS
+
+from quacc.experiments.report import Report
 
 PROBLEM = "binary"
 
 
-def gen_acc_names():
+def get_acc_names():
     yield "vanilla_accuracy"
 
 
@@ -13,8 +21,8 @@ def get_dataset_names():
         return [d for d in UCI_BINARY_DATASETS if d not in _uci_skip]
 
 
-def gen_classifier_names():
-    return ["LR"]
+def get_classifier_names():
+    return ["LR", "SVM(rbf)", "KNN_10", "MLP"]
 
 
 def get_method_names():
@@ -23,13 +31,45 @@ def get_method_names():
         "LEAP(SLD)",
         "OCE(SLD)",
         "PHD(SLD)",
-        "ReQua(SLD-KRR)",
-        "ReQua(SLD-Ridge)",
+        # "ReQua(SLD-KRR)",
+        # "ReQua(SLD-Ridge)",
     ]
 
 
+def get_path(acc_name, cls_name, ext="pdf"):
+    basedir = os.path.join("output", "critdd", PROBLEM)
+    os.makedirs(basedir, exist_ok=True)
+    return os.path.join(basedir, f"critdd_{acc_name}_{cls_name}.{ext}")
+
+
+def clean_aux_files(acc_name, cls_name):
+    log_path = get_path(acc_name, cls_name, ext="log")
+    aux_path = get_path(acc_name, cls_name, ext="aux")
+    pathlib.Path.unlink(log_path)
+    pathlib.Path.unlink(aux_path)
+
+
 def plotting():
-    pass
+    for acc_name, cls_name in IT.product(get_acc_names(), get_classifier_names()):
+        datasets = get_dataset_names()
+        methods = get_method_names()
+
+        df = (
+            Report.load_results(PROBLEM, cls_name, acc_name, datasets, methods)
+            .table_data()
+            .pivot(index="dataset", columns="method", values="acc_err")
+        )
+        diagram = Diagram(df.to_numpy(), treatment_names=df.columns)
+
+        path = get_path(acc_name, cls_name)
+        diagram.to_file(
+            path,
+            alpha=0.05,
+            adjustment="holm",
+            reverse_x=True,
+            axis_options={"title": "critdd"},
+        )
+        clean_aux_files(acc_name, cls_name)
 
 
 if __name__ == "__main__":
