25bb4d3ba6accc7d70b11da7f8def9e221518b06
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-12-19 19:11:35 +0100

    completed calibration error, adaptive binning missing

--- calibration/error.py
+++ calibration/error.py
@@ -14,9 +14,28 @@ def calibration_error(P, y, n_bins=15, adaptive_binning=False, norm="l1"):
     """
     y_hat = np.argmax(P, axis=1)
     bins = np.linspace(0, 1, n_bins + 1)
+    bin_widths = bins[1:] - bins[:-1]
     binning = np.digitize(P[:, y], bins, right=True)
 
-    bin_ce = []
-    for b in np.arange(n_bins + 1)[1:]:
+    bin_ce = np.zeros(n_bins)
+    for b in np.unique(binning):
         b_idx = np.nonzero(binning == b)[0]
         bin_conf_err = (y[b_idx] == y_hat[b_idx]).mean() - P[b_idx, y].mean()
+        bin_ce[b - 1] = bin_conf_err
+
+    if norm == "l1":
+        return np.sum(bin_widths * np.abs(bin_ce))
+    elif norm == "l2":
+        return np.sqrt(np.sum(bin_widths * bin_ce**2))
+    elif norm == "inf":
+        return np.max(bin_ce)
+
+
+if __name__ == "__main__":
+    rng = np.random.default_rng(0)
+    a = rng.random(30)
+    print(a)
+    n_bins = 15
+    bins = np.linspace(0, 1, n_bins + 1)
+    binning = np.digitize(a, bins, right=True)
+    print(binning)
