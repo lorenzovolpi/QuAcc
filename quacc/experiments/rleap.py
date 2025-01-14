57e504504bd6f4cbbb7c91b3683ca5cb9e876486
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-01-14 21:08:53 +0100

    added methods to gen_methods

--- quacc/experiments/rleap.py
+++ quacc/experiments/rleap.py
@@ -12,22 +12,22 @@ from sklearn.linear_model import LogisticRegression
 
 import quacc as qc
 from quacc.data.datasets import fetch_IMDBDataset, fetch_RCV1BinaryDataset
-from quacc.data.util import split_train
 from quacc.error import f1_macro, vanilla_acc
 from quacc.experiments.util import (
     fit_or_switch,
     get_logger,
-    get_plain_prev,
     get_predictions,
-    prevs_from_prot,
     split_validation,
 )
-from quacc.models.cont_table import LEAP
+from quacc.models.cont_table import LEAP, QuAcc1xN2, QuAcc1xNp1, QuAccNxN
+from quacc.models.direct import ATC, DoC
+from quacc.models.model_selection import GridSearchCAP as GSCAP
 from quacc.utils.commons import get_shift, true_acc
 
 qp.environ["SAMPLE_SIZE"] = 1000
 NUM_TEST = 100
 qp.environ["_R_SEED"] = 0
+PROBLEM = "binary"
 
 CSV_SEP = ","
 
@@ -75,10 +75,54 @@ def local_path(dataset_name, cls_name, method_name, acc_name, L: LabelledCollect
     return os.path.join(parent_dir, f"{method_name}.csv")
 
 
-def gen_methods(h):
-    _, acc_fn = next(gen_accs())
+def gen_baselines(acc_fn):
+    yield "ATC-MC", ATC(acc_fn, scoring_fn="maxconf")
+
+
+def gen_baselines_vp(acc_fn, V2_prot, V2_prot_posteriors):
+    yield "DoC", DoC(acc_fn, V2_prot, V2_prot_posteriors)
+
+
+def gen_CAP_cont_table(h, acc_fn):
     yield "LEAP(SLD)", LEAP(acc_fn, sld(), reuse_h=h)
     yield "LEAP(KDEy)", LEAP(acc_fn, kdey(), reuse_h=h)
+
+
+# fmt: off
+def gen_CAP_cont_table_opt(acc_fn, V2_prot, V2_prot_posteriors):
+    pacc_lr_params = {
+        "q_class__classifier__C": np.logspace(-3, 3, 7),
+        "q_class__classifier__class_weight": [None, "balanced"],
+        "add_posteriors": [True, False],
+        "add_y_hat": [True, False],
+        "add_maxconf": [True, False],
+        "add_negentropy": [True, False],
+        "add_maxinfsoft": [True, False],
+    }
+    emq_lr_params = pacc_lr_params | {"q_class__recalib": [None, "bcts"]}
+    kde_lr_params = pacc_lr_params | {"q_class__bandwidth": np.linspace(0.01, 0.2, 5)}
+
+    yield "QuAcc(SLD)1xn2", GSCAP(QuAcc1xN2(acc_fn, sld()), emq_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+    yield "QuAcc(SLD)nxn", GSCAP(QuAccNxN(acc_fn, sld()), emq_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+    if PROBLEM == "binary":
+        yield "QuAcc(SLD)1xnp1", GSCAP(QuAcc1xNp1(acc_fn, sld()), emq_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+    yield "QuAcc(KDEy)1xn2", GSCAP(QuAcc1xN2(acc_fn, kdey()), kde_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+    yield "QuAcc(KDEy)nxn", GSCAP(QuAccNxN(acc_fn, kdey()), kde_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+    if PROBLEM == "binary":
+        yield "QuAcc(KDEy)1xnp1", GSCAP(QuAcc1xNp1(acc_fn, kdey()), kde_lr_params, V2_prot, V2_prot_posteriors, acc_fn, refit=False)
+# fmt: on
+
+
+def gen_methods(h, V, V_posteriors, V1, V1_posteriors, V2_prot, V2_prot_posteriors):
+    _, acc_fn = next(gen_accs())
+    for name, method in gen_baselines(acc_fn, V2_prot, V2_prot_posteriors):
+        yield name, method, V, V_posteriors
+    for name, method in gen_baselines_vp(acc_fn, V2_prot, V2_prot_posteriors):
+        yield name, method, V1, V1_posteriors
+    for name, method in gen_CAP_cont_table(h, acc_fn):
+        yield name, method, V, V_posteriors
+    for name, method in gen_CAP_cont_table_opt(acc_fn, V2_prot, V2_prot_posteriors):
+        yield name, method, V1, V1_posteriors
 
 
 def get_method_names():
@@ -164,7 +208,7 @@ def experiments():
 
                 # for method_name, method in gen_methods(h):
                 for method_name, method, val, val_posteriors in gen_methods(
-                    h, V, V_posteriors, V1, V1_posteriors, V2_prot, V2_prot_posteriors, PROBLEM, MODEL_TYPE, ORACLE
+                    h, V, V_posteriors, V1, V1_posteriors, V2_prot, V2_prot_posteriors
                 ):
                     t_train = None
                     for acc_name, acc_fn in gen_accs():
@@ -177,7 +221,7 @@ def experiments():
                             continue
 
                         try:
-                            method, _t_train = fit_or_switch(method, V, V_posteriors, acc_fn, t_train is not None)
+                            method, _t_train = fit_or_switch(method, val, val_posteriors, acc_fn, t_train is not None)
                             t_train = t_train if _t_train is None else _t_train
                             test_shift = get_shift(np.array([Ui.prevalence() for Ui in test_prot()]), L.prevalence())
                             estim_accs, t_test_ave = get_predictions(method, test_prot, test_prot_posteriors, False)
