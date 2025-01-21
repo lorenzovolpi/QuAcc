28bd63f3fc271e3ff604aa8bb5fe9c5f0fc07844
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-01-21 20:35:09 +0100

    updated selected multiclass datasets for RCV1

--- playground/protocol.py
+++ playground/protocol.py
@@ -1,10 +1,31 @@
 import numpy as np
+import quapy as qp
 from quapy.data.datasets import fetch_UCIBinaryDataset, fetch_UCIMulticlassDataset
 from quapy.functional import uniform_prevalence_sampling, uniform_simplex_sampling
 from quapy.protocol import APP, UPP
+from sklearn.datasets import fetch_rcv1
+
+import quacc as qc
+from quacc.data.datasets import fetch_RCV1MulticlassDataset
+from quacc.data.util import get_rcv1_class_info
+
+qp.environ["_R_SEED"] = 0
+
 
 if __name__ == "__main__":
-    L, U = fetch_UCIMulticlassDataset("digits").train_test
-    prevs_u = uniform_prevalence_sampling(L.n_classes, 9)
-    print(prevs_u)
-    print(L.counts() / prevs_u)
+    # L, U = fetch_UCIMulticlassDataset("digits").train_test
+    class_names, _, _ = get_rcv1_class_info()
+
+    for cn in class_names:
+        try:
+            L, _, _ = fetch_RCV1MulticlassDataset(cn)
+            print(f"{cn} : {L.counts()} - {len(L)}")
+        except KeyError:
+            pass
+
+    # class_names, tree, index = get_rcv1_class_info()
+    #
+    # training = fetch_rcv1(subset="train", data_home=qc.env["SKLEARN_DATA"])
+    # t_labels = extend_labels(training.target.toarray(), training.target_names, class_names)
+    # print(t_labels[:, 0].sum())
+    # print((t_labels[:, [0, 19, 24, 33]].sum(axis=1) > 1).sum())
--- quacc/data/datasets.py
+++ quacc/data/datasets.py
@@ -20,10 +20,12 @@ RCV1_BINARY_DATASETS = [
 RCV1_MULTICLASS_DATASETS = [
     # "C18", "C31", "E51", "M14",  # 3 classes
     # "C17", "C2", "C3", "E1", "M1", "Root",  # 4 classes
-    "C1",  # 8 classes
+    # "C1",  # 8 classes
+    # "ECAT", # 7 classes
+    "Root", # 4 classes
     "CCAT", # 4 classes
-    "ECAT", # 7 classes
     "M1", # 4 classes
+    "M14", # 3 classes
 ]
 HF_DATASETS = ["imdb", "rotten_tomatoes", "amazon_polarity"]
 # fmt: on
