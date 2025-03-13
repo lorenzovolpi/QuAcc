24e22b75ae8bf90fc471d776c9103a5ec4ca63b1
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-03-13 20:00:45 +0100

    torchv2 updated

--- playground/torchv2.py
+++ playground/torchv2.py
@@ -1,8 +1,11 @@
 import os
+from time import time
 
 import numpy as np
 import quapy as qp
 import torch
+from quapy.data import LabelledCollection
+from quapy.protocol import UPP
 from sklearn.base import BaseEstimator
 
 qp.environ["_R_SEED"] = 0
@@ -10,14 +13,15 @@ qp.environ["_R_SEED"] = 0
 
 class BaseEstimatorAdapter(BaseEstimator):
     def __init__(self, V_hidden_states, U_hidden_states, V_logits, U_logits):
-        self.V_hs = V_hidden_states
-        self.U_hs = U_hidden_states
-        self.V_logits = V_logits
-        self.U_logits = U_logits
+        self.hs = np.vstack([V_hidden_states, U_hidden_states])
+        self.logits = np.vstack([V_logits, U_logits])
 
     def decision_function(self, X: np.ndarray):
-        print(X[:, np.newaxis, :])
-        print(np.nonzero((X[:, np.newaxis, :] == self.V_hs).all(axis=-1).sum(axis=0))[0])
+        nzs_row, nzs_col = np.nonzero((X[:, np.newaxis, :] == self.hs).all(axis=-1))
+        found_row_idx = np.searchsorted(nzs_row, np.unique(nzs_row))
+        logits_idx = nzs_col[found_row_idx]
+        print(logits_idx)
+        return self.logits[logits_idx, :]
 
         # def find_idx(a):
         #     return np.nonzero((self.V_hs == a).all(axis=-1))[0][0]
@@ -36,8 +40,14 @@ if __name__ == "__main__":
     U_hidden_states = torch.load(os.path.join(parent_dir, "hidden_states.test.pt")).numpy()
     U_logits = torch.load(os.path.join(parent_dir, "logits.test.pt")).numpy()
 
+    V = LabelledCollection(V_hidden_states, np.argmax(V_logits, axis=-1), [0, 1])
+    U = LabelledCollection(U_hidden_states, np.argmax(U_logits, axis=-1), [0, 1])
+
     model = BaseEstimatorAdapter(V_hidden_states, U_hidden_states, V_logits, U_logits)
 
-    test_X = V_hidden_states[[12, 115, 257], :]
-    print(test_X)
-    model.decision_function(test_X)
+    prot = UPP(U, sample_size=1000, repeats=100, random_state=0, return_type="labelled_collection")
+
+    start = time()
+    V_P = model.decision_function(V.X)
+    U_Ps = [model.decision_function(Ui.X) for Ui in prot()]
+    print(time() - start)
