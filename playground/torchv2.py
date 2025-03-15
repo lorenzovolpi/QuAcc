75ad97c6ff04e22915b5bd2b67f4aff1e5261766
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-03-15 21:44:44 +0100

    adapter complete

--- playground/torchv2.py
+++ playground/torchv2.py
@@ -1,11 +1,14 @@
 import os
+from collections import defaultdict
 from time import time
 
 import numpy as np
 import quapy as qp
 import torch
 from quapy.data import LabelledCollection
+from quapy.method.aggregative import KDEyML
 from quapy.protocol import UPP
+from scipy.special import softmax
 from sklearn.base import BaseEstimator
 
 qp.environ["_R_SEED"] = 0
@@ -16,18 +19,28 @@ class BaseEstimatorAdapter(BaseEstimator):
         self.hs = np.vstack([V_hidden_states, U_hidden_states])
         self.logits = np.vstack([V_logits, U_logits])
 
-    def decision_function(self, X: np.ndarray):
-        nzs_row, nzs_col = np.nonzero((X[:, np.newaxis, :] == self.hs).all(axis=-1))
-        found_row_idx = np.searchsorted(nzs_row, np.unique(nzs_row))
-        logits_idx = nzs_col[found_row_idx]
-        print(logits_idx)
-        return self.logits[logits_idx, :]
+        hashes = self._hash(self.hs)
+        self._dict = defaultdict(lambda: [])
+        for i, hash in enumerate(hashes):
+            self._dict[hash].append(i)
 
-        # def find_idx(a):
-        #     return np.nonzero((self.V_hs == a).all(axis=-1))[0][0]
-        #
-        # idxs = np.vectorize(find_idx, signature="(m,n)->(m)")(X)
-        # print(idxs)
+    def _hash(self, X):
+        return np.around(np.abs(X).sum(axis=-1) * self.hs.shape[0])
+
+    def predict_proba(self, X: np.ndarray):
+        def f(data, hash):
+            _ids = np.array(self._dict[hash])
+            _m = self.hs[_ids, :]
+            _eq_idx = np.nonzero((_m == data).all(axis=-1))[0][0]
+            return _ids[_eq_idx]
+
+        hashes = self._hash(X)
+        logits_idx = np.vectorize(f, signature="(m),()->()")(X, hashes)
+        _logits = self.logits[logits_idx, :]
+        return softmax(_logits, axis=-1)
+
+    def decision_function(self, X: np.ndarray):
+        return self.predict_proba(X)
 
 
 if __name__ == "__main__":
@@ -47,7 +60,16 @@ if __name__ == "__main__":
 
     prot = UPP(U, sample_size=1000, repeats=100, random_state=0, return_type="labelled_collection")
 
-    start = time()
+    t0 = time()
     V_P = model.decision_function(V.X)
+    print(V_P)
+    print(V_P.shape, type(V_P))
+    t1 = time()
+    print("val.:", f"{t1 - t0:.3f}s")
     U_Ps = [model.decision_function(Ui.X) for Ui in prot()]
-    print(time() - start)
+    t2 = time()
+    print("test (avg.):", f"{(t2 - t1) / prot.repeats:.3f}s")
+
+    q = KDEyML(model)
+    q.fit(V, fit_classifier=False)
+    print([q.quantify(Ui.X) for Ui in prot()])
