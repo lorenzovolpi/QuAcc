2afeddbef3851f9ade234eebdf9827c383266585
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-03-25 14:23:53 +0100

    created OracleQuantifier

--- quacc/models/utils.py
+++ quacc/models/utils.py
@@ -1,5 +1,10 @@
+from collections import defaultdict
+from typing import List, override
+
 import numpy as np
 import scipy
+from quapy.data import LabelledCollection
+from quapy.method.aggregative import AggregativeQuantifier
 
 
 def get_posteriors_from_h(h, X):
@@ -44,3 +49,33 @@ def smooth(prevalences, epsilon=1e-5, axis=None):
     prevalences = prevalences + epsilon
     prevalences /= prevalences.sum(axis=axis, keepdims=axis is not None)
     return prevalences
+
+
+class OracleQuantifier(AggregativeQuantifier):
+    def __init__(self, labelled_data: List[LabelledCollection]):
+        self.hash_K = len(labelled_data)
+        self.data_map = defaultdict(lambda: [])
+        for ui in labelled_data:
+            self.data_map[self._get_hash(ui.X)].append(ui)
+
+    def _get_hash(self, X):
+        return float(np.around(np.abs(X.prod(axis=1)).sum() * self.hash_K))
+
+    @override
+    def fit(self, data: LabelledCollection, fit_classifier=True, val_split=None):
+        pass
+
+    @override
+    def aggregation_fit(self, classif_predictions: LabelledCollection, data: LabelledCollection):
+        pass
+
+    @override
+    def aggregate(self, classif_predictions: np.ndarray):
+        pass
+
+    @override
+    def quantify(self, instances):
+        _hash = self._get_hash(instances)
+        lcs = self.data_map[_hash]
+        eq_idx = [np.all(instances == lc.X) for lc in lcs].index(True)
+        return lcs[eq_idx].prevalence()
