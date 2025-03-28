2eaab151962c61668ade8f77c5cb3fb970f3afa1
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-03-28 14:01:13 +0100

    fixed palette and ratio for plots (leap)

--- exp/leap/plot.py
+++ exp/leap/plot.py
@@ -1,6 +1,7 @@
 import itertools as IT
 import os
 
+import numpy as np
 import seaborn as sns
 
 from exp.leap.config import PROBLEM, get_acc_names, get_classifier_names, get_dataset_names, root_dir
@@ -11,6 +12,8 @@ method_map = {
     "LEAP(KDEy)": "LEAP$_{\\mathrm{KDEy}}$",
     "PHD(KDEy)": "S-LEAP$_{\\mathrm{KDEy}}$",
     "OCE(KDEy)-SLSQP": "O-LEAP$_{\\mathrm{KDEy}}$",
+    "LEAP(CC)": "LEAP$_{\\mathrm{CC}}$",
+    "LEAP(oracle)": "LEAP$_{\\Phi}$",
 }
 
 dataset_map = {
@@ -28,41 +31,79 @@ def get_selection_datasets():
         return ["phishing", "page_block", "academic-success", "mhr"]
 
 
+def get_palette():
+    base_palette = sns.color_palette("bright", 10)
+    _dict = {
+        "DoC": 0,
+        "LEAP(KDEy)": 1,
+        "PHD(KDEy)": 2,
+        "OCE(KDEy)-SLSQP": 3,
+        "LEAP(CC)": 4,
+        "LEAP(oracle)": 7,
+    }
+    return {method_map.get(k, k): base_palette[v] for k, v in _dict.items()}
+
+
 def plots():
     res = load_results()
 
     classifiers = get_classifier_names()
     accs = get_acc_names()
-    methods = ["DoC", "LEAP(KDEy)", "PHD(KDEy)", "OCE(KDEy)-SLSQP"]
+    main_methods = ["DoC", "LEAP(KDEy)", "PHD(KDEy)", "OCE(KDEy)-SLSQP"]
+    oracle_methods = ["LEAP(CC)", "LEAP(KDEy)", "LEAP(oracle)"]
 
-    dataset_configs = {
-        "all": get_dataset_names(),
-        "4x1": get_selection_datasets(),
-    }
+    configs = [
+        {
+            "name": "all",
+            "datasets": get_dataset_names(),
+            "methods": main_methods,
+        },
+        {
+            "name": "4x1",
+            "datasets": get_selection_datasets(),
+            "methods": main_methods,
+        },
+        {
+            "name": "all_oracle",
+            "datasets": get_dataset_names(),
+            "methods": oracle_methods,
+        },
+        {
+            "name": "4x1_oracle",
+            "datasets": get_selection_datasets(),
+            "methods": oracle_methods,
+        },
+    ]
 
     parent_dir = os.path.join(root_dir, "plots")
     os.makedirs(parent_dir, exist_ok=True)
 
     for cls_name, acc in IT.product(classifiers[:1], accs):
         print(f"Plotting {cls_name} for {acc}")
+        df = res.loc[(res["classifier"] == cls_name) & (res["acc_name"] == acc), :]
 
-        df = res.loc[(res["classifier"] == cls_name) & (res["acc_name"] == acc) & (res["method"].isin(methods)), :]
-        df, _methods = rename_methods(method_map, df, methods)
+        for cfg in configs:
+            name, datasets, methods = cfg["name"], cfg["datasets"], cfg["methods"]
+            dfc = df.loc[(df["dataset"].isin(datasets)) & (df["method"].isin(methods)), :]
+            dfc, methods = rename_methods(method_map, dfc, methods)
+            dfc, datasets = rename_datasets(dataset_map, dfc, datasets)
 
-        for config, datasets in dataset_configs.items():
-            df, _datasets = rename_datasets(dataset_map, df, datasets)
-            df_config = df.loc[df["dataset"].isin(_datasets), :]
             plot_diagonal_grid(
-                df_config,
-                _methods,
+                dfc,
+                methods,
                 basedir=parent_dir,
-                filename=f"grid_{cls_name}_{PROBLEM}_{config}",
+                filename=f"grid_{cls_name}_{PROBLEM}_{name}",
                 n_cols=4,
                 legend_bbox_to_anchor=(0.95, 0.3),
-                palette="deep",
+                # palette="deep",
                 # palette=sns.color_palette("hls", len(_methods)),
+                palette=get_palette(),
                 x_label="True Accuracy",
                 y_label="Estimated Accuracy",
+                aspect=0.8,
+                xtick_vert=True,
+                xticks=np.linspace(0, 1, 6, endpoint=True),
+                yticks=np.linspace(0, 1, 6, endpoint=True),
             )
 
 
