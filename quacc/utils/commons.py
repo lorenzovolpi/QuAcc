239acbfe4abe7cd705c5d11e67d96437167536f6
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-03-29 14:30:17 +0100

    added return_as option to parallel function

--- quacc/utils/commons.py
+++ quacc/utils/commons.py
@@ -3,7 +3,7 @@ import json
 import os
 from contextlib import ExitStack, contextmanager
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Literal
 from urllib.request import urlretrieve
 
 import numpy as np
@@ -150,7 +150,17 @@ def temp_force_njobs(force):
                 os.environ.pop("OPENBLAS_NUM_THREADS")
 
 
-def parallel(func, args_list, n_jobs, seed=None, asarray=True, backend="loky", verbose=0, batch_size="auto"):
+def parallel(
+    func,
+    args_list,
+    n_jobs,
+    seed=None,
+    asarray=True,
+    return_as: Literal["list"] | Literal["generator"] | Literal["generator_unordered"] = "list",
+    backend="loky",
+    verbose=0,
+    batch_size="auto",
+):
     """
     A wrapper of multiprocessing:
 
@@ -181,10 +191,13 @@ def parallel(func, args_list, n_jobs, seed=None, asarray=True, backend="loky", v
 
     with ExitStack() as stack:
         stack.enter_context(qc.commons.temp_force_njobs(qc.env["FORCE_NJOBS"]))
-        out = Parallel(n_jobs=n_jobs, backend=backend, verbose=verbose, batch_size=batch_size)(
+        out = Parallel(n_jobs=n_jobs, return_as=return_as, backend=backend, verbose=verbose, batch_size=batch_size)(
             delayed(func_dec)(qp.environ, qc.env, None if seed is None else seed + i, args_i)
             for i, args_i in enumerate(args_list)
         )
+
+    if return_as != "list":
+        asarray = False
 
     if asarray:
         out = np.asarray(out)
