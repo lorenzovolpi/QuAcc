8355e3fa92edd60d0b53736f68dd6596c6078257
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-04-10 14:43:22 +0200

    created elaborating function (trd)

--- exp/trd/util.py
+++ exp/trd/util.py
@@ -1,9 +1,11 @@
 import glob
+import itertools as IT
 import os
 
 import pandas as pd
 
 from exp.leap.config import PROBLEM, root_dir
+from exp.trd.config import get_acc_names, get_CAP_method_names
 
 
 def local_path(dataset_name, cls_name, method_name, acc_name):
@@ -18,3 +20,17 @@ def load_results(filter_methods=None) -> pd.DataFrame:
         dfs.append(pd.read_json(path))
 
     return pd.concat(dfs, axis=0)
+
+
+def elab(res: pd.DataFrame):
+    methods = get_CAP_method_names()
+    accs = get_acc_names()
+
+    dfs = []
+
+    for acc_name, method in IT.product(accs, methods):
+        df = res.loc[(res["method"] == method) & (res["acc_name"] == acc_name), :].copy()
+
+        best_samples_idx = df.groupby(["uids", "dataset"])["estim_accs"].idxmin()
+        df = df.loc[best_samples_idx].reset_index(drop=False)
+        print(df)
