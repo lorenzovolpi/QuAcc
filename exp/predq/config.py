873e1fbb60e420ef16c29c213b813ef46831355b
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-05-16 15:10:20 +0200

    added predq implementation

---
+++ exp/predq/config.py
@@ -0,0 +1,177 @@
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import quapy as qp
+from quapy.data import LabelledCollection
+from quapy.data.datasets import UCI_BINARY_DATASETS, UCI_MULTICLASS_DATASETS
+from quapy.method.aggregative import KDEyML
+from quapy.protocol import UPP, AbstractStochasticSeededProtocol
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.neural_network import MLPClassifier as MLP
+from sklearn.svm import SVC
+
+import quacc as qc
+from exp.util import split_validation
+from quacc.data.datasets import fetch_UCIBinaryDataset, fetch_UCIMulticlassDataset, sort_datasets_by_size
+from quacc.error import f1, f1_macro, vanilla_acc
+from quacc.models.cont_table import LEAP, OCE, PHD, NaiveCAP
+from quacc.models.direct import ATC, DoC
+from quacc.models.utils import OracleQuantifier
+from quacc.utils.commons import contingency_table
+
+PROJECT = "predq"
+root_dir = os.path.join(qc.env["OUT_DIR"], PROJECT)
+NUM_TEST = 1000
+qp.environ["_R_SEED"] = 0
+
+PROBLEM = "binary"
+
+_toggle = {
+    "vanilla": True,
+    "f1": False,
+}
+
+
+@dataclass
+class DatasetBundle:
+    L_prevalence: np.ndarray
+    V: LabelledCollection
+    U: LabelledCollection
+    V1: LabelledCollection = None
+    V2_prot: AbstractStochasticSeededProtocol = None
+    test_prot: AbstractStochasticSeededProtocol = None
+    V_posteriors: np.ndarray = None
+    V1_posteriors: np.ndarray = None
+    V2_prot_posteriors: np.ndarray = None
+    test_prot_posteriors: np.ndarray = None
+    test_prot_y_hat: np.ndarray = None
+    test_prot_true_cts: np.ndarray = None
+
+    def create_bundle(self, h: BaseEstimator):
+        # generate test protocol
+        self.test_prot = UPP(
+            self.U,
+            repeats=NUM_TEST,
+            return_type="labelled_collection",
+            random_state=qp.environ["_R_SEED"],
+        )
+
+        # split validation set
+        self.V1, self.V2_prot = split_validation(self.V)
+
+        # precomumpute model posteriors for validation sets
+        self.V_posteriors = h.predict_proba(self.V.X)
+        self.V1_posteriors = h.predict_proba(self.V1.X)
+        self.V2_prot_posteriors = []
+        for sample in self.V2_prot():
+            self.V2_prot_posteriors.append(h.predict_proba(sample.X))
+
+        # precomumpute model posteriors for test samples
+        self.test_prot_posteriors, self.test_prot_y_hat, self.test_prot_true_cts = [], [], []
+        for sample in self.test_prot():
+            P = h.predict_proba(sample.X)
+            self.test_prot_posteriors.append(P)
+            y_hat = np.argmax(P, axis=-1)
+            self.test_prot_y_hat.append(y_hat)
+            self.test_prot_true_cts.append(contingency_table(sample.y, y_hat, sample.n_classes))
+
+        return self
+
+    @classmethod
+    def mock(cls):
+        return DatasetBundle(None, None, None, test_prot=lambda: [])
+
+
+def kdey():
+    return KDEyML(MLP())
+
+
+def gen_classifiers():
+    yield "LR", LogisticRegression()
+    yield "kNN", KNN(n_neighbors=10)
+    yield "SVM", SVC(kernel="rbf", probability=True)
+    yield "MLP", MLP()
+
+
+def gen_datasets(only_names=False):
+    if PROBLEM == "binary":
+        _uci_skip = ["acute.a", "acute.b", "balance.2", "iris.1"]
+        _uci_names = [d for d in UCI_BINARY_DATASETS if d not in _uci_skip]
+        _sorted_uci_names = sort_datasets_by_size(_uci_names, fetch_UCIBinaryDataset)
+        for dn in _sorted_uci_names:
+            dval = None if only_names else fetch_UCIBinaryDataset(dn)
+            yield dn, dval
+    elif PROBLEM == "multiclass":
+        _uci_skip = ["isolet", "wine-quality", "letter"]
+        _uci_names = [d for d in UCI_MULTICLASS_DATASETS if d not in _uci_skip]
+        _sorted_uci_names = sort_datasets_by_size(_uci_names, fetch_UCIMulticlassDataset)
+        for dataset_name in _sorted_uci_names:
+            dval = None if only_names else fetch_UCIMulticlassDataset(dataset_name)
+            yield dataset_name, dval
+
+
+def gen_acc_measure():
+    multiclass = PROBLEM == "multiclass"
+    if _toggle["vanilla"]:
+        yield "vanilla_accuracy", vanilla_acc
+    if _toggle["f1"]:
+        yield "macro-F1", f1_macro if multiclass else f1
+
+
+def gen_baselines(acc_fn):
+    yield "Naive", NaiveCAP(acc_fn)
+    yield "ATC-MC", ATC(acc_fn, scoring_fn="maxconf")
+
+
+def gen_baselines_vp(acc_fn, D):
+    yield "DoC", DoC(acc_fn, D.V2_prot, D.V2_prot_posteriors)
+
+
+def gen_CAP_cont_table(h, acc_fn):
+    yield "LEAP(KDEy)", LEAP(acc_fn, kdey())
+    yield "S-LEAP(KDEy)", PHD(acc_fn, kdey())
+    yield "O-LEAP(KDEy)", OCE(acc_fn, kdey(), optim_method="SLSQP")
+
+
+def gen_methods_with_oracle(h, acc_fn, D: DatasetBundle):
+    oracle_q = OracleQuantifier([ui for ui in D.test_prot()])
+    yield "LEAP(KDEy)", LEAP(acc_fn, oracle_q)
+    yield "S-LEAP(KDEy)", PHD(acc_fn, oracle_q)
+    yield "O-LEAP(oracle)", OCE(acc_fn, oracle_q, reuse_h=h, optim_method="SLSQP")
+
+
+def gen_methods(h, D, with_oracle=False):
+    _, acc_fn = next(gen_acc_measure())
+    for name, method in gen_baselines(acc_fn):
+        yield name, method, D.V, D.V_posteriors
+    for name, method in gen_baselines_vp(acc_fn, D):
+        yield name, method, D.V1, D.V1_posteriors
+    for name, method in gen_CAP_cont_table(h, acc_fn):
+        yield name, method, D.V, D.V_posteriors
+    if with_oracle:
+        for name, method in gen_methods_with_oracle(h, acc_fn, D):
+            yield name, method, D.V, D.V_posteriors
+
+
+def get_classifier_names():
+    return [clsf.name for clsf in gen_classifiers()]
+
+
+def get_dataset_names():
+    return [name for name, _ in gen_datasets(only_names=True)]
+
+
+def get_acc_names():
+    return [acc_name for acc_name, _ in gen_acc_measure()]
+
+
+def get_method_names(with_oracle=False):
+    mock_h = LogisticRegression()
+    mock_D = DatasetBundle.mock()
+
+    names = [m for m, _ in gen_methods(mock_h, mock_D, with_oracle=with_oracle)]
+
+    return names
---
+++ exp/predq/main.py
@@ -0,0 +1,240 @@
+import itertools as IT
+import os
+from dataclasses import dataclass
+from traceback import print_exception
+
+import numpy as np
+import pandas as pd
+import quapy as qp
+from sklearn.base import clone as skl_clone
+
+import quacc as qc
+from exp.predq.config import (
+    PROJECT,
+    DatasetBundle,
+    gen_acc_measure,
+    gen_classifiers,
+    gen_datasets,
+    gen_methods,
+    get_method_names,
+)
+from exp.trd.util import local_path
+from exp.util import (
+    fit_or_switch,
+    gen_model_dataset,
+    get_ct_predictions,
+    get_logger,
+    get_plain_prev,
+    timestamp,
+)
+from quacc.models.cont_table import LEAP
+from quacc.utils.commons import get_shift, parallel, true_acc
+
+log = get_logger(id=PROJECT)
+
+qp.environ["SAMPLE_SIZE"] = 100
+
+
+def is_excluded(classifier, dataset, method, acc):
+    return False
+
+
+def get_extra_from_method(df, method):
+    if isinstance(method, LEAP):
+        df["true_solve"] = method._true_solve_log[-1]
+
+
+def all_exist_pre_check(dataset_name, cls_name):
+    method_names = get_method_names()
+    acc_names = [acc_name for acc_name, _ in gen_acc_measure()]
+
+    all_exist = True
+    for method, acc in IT.product(method_names, acc_names):
+        if is_excluded(cls_name, dataset_name, method, acc):
+            continue
+        path = local_path(dataset_name, cls_name, method, acc)
+        all_exist = os.path.exists(path)
+        if not all_exist:
+            break
+
+    return all_exist
+
+
+def gen_method_df(df_len, **data):
+    data = data | {k: [v] * df_len for k, v in data.items() if not isinstance(v, list)}
+    return pd.DataFrame.from_dict(data, orient="columns")
+
+
+@dataclass
+class EXP:
+    code: int
+    cls_name: str
+    dataset_name: str
+    acc_name: str
+    method_name: str
+    df: pd.DataFrame = None
+    t_train: float = None
+    t_test_ave: float = None
+    err: Exception = None
+
+    @classmethod
+    def SUCCESS(cls, *args, **kwargs):
+        return EXP(200, *args, **kwargs)
+
+    @classmethod
+    def EXISTS(cls, *args, **kwargs):
+        return EXP(300, *args, **kwargs)
+
+    @classmethod
+    def ERROR(cls, e, *args, **kwargs):
+        return EXP(400, *args, err=e, **kwargs)
+
+    @property
+    def ok(self):
+        return self.code == 200
+
+    @property
+    def old(self):
+        return self.code == 300
+
+    def error(self):
+        return self.code == 400
+
+
+def exp_protocol(args):
+    cls_name, dataset_name, h, D, true_accs, method_name, method, val, val_posteriors = args
+    results = []
+
+    L_prev = get_plain_prev(D.L_prevalence)
+    val_prev = get_plain_prev(val.prevalence())
+    t_train = None
+    for acc_name, acc_fn in gen_acc_measure():
+        if is_excluded(cls_name, dataset_name, method_name, acc_name):
+            continue
+        path = local_path(dataset_name, cls_name, method_name, acc_name)
+        if os.path.exists(path):
+            results.append(EXP.EXISTS(cls_name, dataset_name, acc_name, method_name))
+            continue
+
+        try:
+            method, _t_train = fit_or_switch(method, val, val_posteriors, acc_fn, t_train is not None)
+            t_train = t_train if _t_train is None else _t_train
+
+            test_shift = get_shift(np.array([Ui.prevalence() for Ui in D.test_prot()]), D.L_prevalence).tolist()
+            estim_accs, estim_cts, t_test_ave = get_ct_predictions(method, D.test_prot, D.test_prot_posteriors)
+            if estim_cts is None:
+                estim_cts = [None] * len(estim_accs)
+            else:
+                estim_cts = [ct.tolist() for ct in estim_cts]
+        except Exception as e:
+            print_exception(e)
+            results.append(EXP.ERROR(e, cls_name, dataset_name, acc_name, method_name))
+            continue
+
+        ae = qc.error.ae(np.array(true_accs[acc_name]), np.array(estim_accs)).tolist()
+
+        df_len = len(estim_accs)
+        method_df = gen_method_df(
+            df_len,
+            shifts=test_shift,
+            true_accs=true_accs[acc_name],
+            estim_accs=estim_accs,
+            acc_err=ae,
+            estim_cts=estim_cts,
+            true_cts=D.test_prot_true_cts,
+            classifier=cls_name,
+            method=method_name,
+            dataset=dataset_name,
+            acc_name=acc_name,
+            train_prev=[L_prev] * df_len,
+            val_prev=[val_prev] * df_len,
+            t_train=t_train,
+            t_test_ave=t_test_ave,
+        )
+        get_extra_from_method(method_df, method)
+
+        results.append(
+            EXP.SUCCESS(
+                cls_name, dataset_name, acc_name, method_name, df=method_df, t_train=t_train, t_test_ave=t_test_ave
+            )
+        )
+
+    return results
+
+
+def train_cls(args):
+    (cls_name, orig_h), (dataset_name, (L, V, U)) = args
+    #
+    # check if all results for current combination already exist
+    # if so, skip the combination
+    if all_exist_pre_check(dataset_name, cls_name):
+        return (cls_name, dataset_name, None, None, None)
+    else:
+        # clone model from the original one
+        h = skl_clone(orig_h)
+        # fit model
+        h.fit(*L.Xy)
+        # create dataset bundle
+        D = DatasetBundle(L.prevalence(), V, U).create_bundle(h)
+        # compute true accs for h on dataset
+        true_accs = {}
+        for acc_name, acc_fn in gen_acc_measure():
+            true_accs[acc_name] = [true_acc(h, acc_fn, Ui) for Ui in D.test_prot()]
+        # store h-dataset combination
+        return (cls_name, dataset_name, h, D, true_accs)
+
+
+def experiments():
+    cls_train_args = list(gen_model_dataset(gen_classifiers, gen_datasets))
+    cls_dataset_gen = parallel(
+        func=train_cls,
+        args_list=cls_train_args,
+        n_jobs=qc.env["N_JOBS"],
+        return_as="generator_unordered",
+    )
+    cls_dataset = []
+    for cls_name, dataset_name, h, D, true_accs in cls_dataset_gen:
+        if h is None and D is None:
+            log.info(f"All results for {cls_name} over {dataset_name} exist, skipping")
+        else:
+            log.info(f"Trained {cls_name} over {dataset_name}")
+            cls_dataset.append((cls_name, dataset_name, h, D, true_accs))
+
+    exp_prot_args_list = []
+    for cls_name, dataset_name, h, D, true_accs in cls_dataset:
+        for method_name, method, val, val_posteriors in gen_methods(h, D):
+            exp_prot_args_list.append(
+                (cls_name, dataset_name, h, D, true_accs, method_name, method, val, val_posteriors)
+            )
+
+    results_gen = parallel(
+        func=exp_protocol,
+        args_list=exp_prot_args_list,
+        n_jobs=qc.env["N_JOBS"],
+        return_as="generator_unordered",
+    )
+
+    for res in results_gen:
+        for r in res:
+            if r.ok:
+                path = local_path(r.dataset_name, r.cls_name, r.method_name, r.acc_name)
+                r.df.to_json(path)
+                log.info(
+                    f"[{r.cls_name}@{r.dataset_name}] {r.method_name} on {r.acc_name} done [{timestamp(r.t_train, r.t_test_ave)}]"
+                )
+            elif r.old:
+                log.info(f"[{r.cls_name}@{r.dataset_name}] {r.method_name} on {r.acc_name} exists, skipping")
+            elif r.error:
+                log.warning(
+                    f"[{r.cls_name}@{r.dataset_name}] {r.method_name}: {r.acc_name} gave error '{r.err}' - skipping"
+                )
+
+
+if __name__ == "__main__":
+    try:
+        log.info("-" * 31 + "  start  " + "-" * 31)
+        experiments()
+        log.info("-" * 32 + "  end  " + "-" * 32)
+    except Exception as e:
+        log.error(e)
+        print_exception(e)
