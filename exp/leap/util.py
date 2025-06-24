7117adb3813bada9fd3fa1b5d6cf67a421db437e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-06-25 01:18:51 +0200

    refactored util methods

--- exp/leap/util.py
+++ exp/leap/util.py
@@ -5,12 +5,14 @@ from pathlib import Path
 import pandas as pd
 
 from exp.leap.config import PROBLEM, get_method_names, root_dir
+from quacc.models.cont_table import LEAP
 
 
-def load_results(filter_methods=None) -> pd.DataFrame:
+def load_results(base_dir=None, filter_methods=None) -> pd.DataFrame:
+    base_dir = root_dir if base_dir is None else base_dir
     dfs = []
     _methods = get_method_names() if filter_methods is None else filter_methods
-    for path in glob.glob(os.path.join(root_dir, PROBLEM, "**", "*.json"), recursive=True):
+    for path in glob.glob(os.path.join(base_dir, PROBLEM, "**", "*.json"), recursive=True):
         if Path(path).stem in _methods:
             dfs.append(pd.read_json(path))
 
@@ -34,3 +36,17 @@ def rename_methods(mapping, df, methods, baselines=None):
     else:
         _baselines = [mapping.get(b, b) for b in baselines]
         return df, _methods, _baselines
+
+
+def is_excluded(classifier, dataset, method, acc):
+    return False
+
+
+def get_extra_from_method(df, method):
+    if isinstance(method, LEAP):
+        df["true_solve"] = method._true_solve_log[-1]
+
+
+def gen_method_df(df_len, **data):
+    data = data | {k: [v] * df_len for k, v in data.items() if not isinstance(v, list)}
+    return pd.DataFrame.from_dict(data, orient="columns")
