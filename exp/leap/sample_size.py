f55d00bf819a8ae3c1dedbfc0447bad687f2ccef
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-16 12:55:19 +0200

    sample_size experiment completed (leap)

--- exp/leap/sample_size.py
+++ exp/leap/sample_size.py
@@ -7,80 +7,102 @@ import numpy as np
 import pandas as pd
 import quapy as qp
 from quapy.data.datasets import UCI_BINARY_DATASETS, UCI_MULTICLASS_DATASETS
+from sklearn.base import BaseEstimator
 from sklearn.base import clone as skl_clone
+from sklearn.linear_model import LogisticRegression
 
+import exp.leap.config as cfg
+import exp.leap.env as env
 import quacc as qc
-from exp.leap.config import (
-    PROBLEM,
-    PROJECT,
-    DatasetBundle,
-    gen_acc_measure,
-    gen_classifiers,
-    gen_methods,
-    get_method_names,
-    root_dir,
-)
-from exp.leap.util import gen_method_df, get_extra_from_method, is_excluded
+from exp.leap.config import DatasetBundle, is_excluded, kdey
+from exp.leap.util import all_exist_pre_check, gen_method_df, get_extra_from_method, local_path
 from exp.util import (
     fit_or_switch,
-    gen_model_dataset,
     get_ct_predictions,
     get_logger,
     get_plain_prev,
     timestamp,
 )
 from quacc.data.datasets import fetch_UCIBinaryDataset, fetch_UCIMulticlassDataset, sort_datasets_by_size
+from quacc.error import vanilla_acc
+from quacc.models.cont_table import OCE
+from quacc.models.direct import DoC
 from quacc.utils.commons import get_shift, parallel, true_acc
 
-log = get_logger(id=PROJECT)
+SUBPROJECT = "sample_size"
+
+log = get_logger(id=f"{env.PROJECT}.{SUBPROJECT}")
 
 
-def local_path(dataset_name, sample_size, cls_name, method_name, acc_name):
-    parent_dir = os.path.join(root_dir, "sample_size", PROBLEM, cls_name, acc_name, f"{dataset_name}_{sample_size}")
-    os.makedirs(parent_dir, exist_ok=True)
-    return os.path.join(parent_dir, f"{method_name}.json")
-
-
-def all_exist_pre_check(dataset_name, sample_size, cls_name):
-    method_names = get_method_names()
-    acc_names = [acc_name for acc_name, _ in gen_acc_measure()]
-
-    all_exist = True
-    for method, acc in IT.product(method_names, acc_names):
-        path = local_path(dataset_name, sample_size, cls_name, method, acc)
-        all_exist = os.path.exists(path)
-        if not all_exist:
-            break
-
-    return all_exist
+def dataset_full_name(dataset_name, sample_size):
+    return f"{dataset_name}_{sample_size}"
 
 
 def gen_sample_sizes():
-    if PROBLEM == "binary":
-        return np.unique(np.around(np.hstack([np.logspace(1, 4, 20), np.logspace(1, 4, 4)]), decimals=0).astype("int"))
-    elif PROBLEM == "multiclass":
-        return np.unique(np.around(np.hstack([np.logspace(1, 5, 20), np.logspace(1, 5, 5)]), decimals=0).astype("int"))
+    return np.unique(np.around(np.hstack([np.logspace(1, 4, 20), np.logspace(1, 4, 4)]), decimals=0).astype("int"))
+    # if cfg.PROBLEM == "binary":
+    #     return np.unique(np.around(np.hstack([np.logspace(1, 4, 20), np.logspace(1, 4, 4)]), decimals=0).astype("int"))
+    # elif cfg.PROBLEM == "multiclass":
+    #     return np.unique(
+    #         np.around(np.hstack([np.logspace(1, 4.4, 20)[:-1], np.logspace(1, 4, 4), [2.5 * 1e4]]), decimals=0).astype(
+    #             "int"
+    #         )
+    #     )
 
 
 def gen_datasets(only_names=False):
-    if PROBLEM == "binary":
+    N_DATASETS = 3
+    if env.PROBLEM == "binary":
         _uci_skip = ["acute.a", "acute.b", "balance.2", "iris.1"]
         _uci_names = [d for d in UCI_BINARY_DATASETS if d not in _uci_skip]
         _sorted_uci_names = sort_datasets_by_size(_uci_names, fetch_UCIBinaryDataset)
-        dn = _sorted_uci_names[0]
-        dval = None if only_names else fetch_UCIBinaryDataset(dn)
-        yield dn, dval
-    elif PROBLEM == "multiclass":
+        for dn in _sorted_uci_names[:N_DATASETS]:
+            dval = None if only_names else fetch_UCIBinaryDataset(dn)
+            yield dn, dval
+    elif env.PROBLEM == "multiclass":
         _uci_skip = ["isolet", "wine-quality", "letter"]
         _uci_names = [d for d in UCI_MULTICLASS_DATASETS if d not in _uci_skip]
         _sorted_uci_names = sort_datasets_by_size(_uci_names, fetch_UCIMulticlassDataset)
-        dn = _sorted_uci_names[0]
-        dval = None if only_names else fetch_UCIMulticlassDataset(dn)
-        yield dn, dval
+        for dn in _sorted_uci_names[:N_DATASETS]:
+            dval = None if only_names else fetch_UCIMulticlassDataset(dn)
+            yield dn, dval
+
+
+def gen_methods(h: BaseEstimator, D: DatasetBundle):
+    _, acc_fn = next(gen_acc_measure())
+    yield "O-LEAP(KDEy-MLP)", OCE(acc_fn, kdey()), D.V, D.V_posteriors
+    yield "DoC", DoC(acc_fn, D.V2_prot, D.V2_prot_posteriors), D.V1, D.V1_posteriors
+
+
+def gen_acc_measure():
+    yield "vanilla_accuracy", vanilla_acc
+
+
+def get_method_names():
+    _, mock_acc_fn = next(gen_acc_measure())
+    mock_h = LogisticRegression()
+    mock_D = DatasetBundle.mock()
+    return [m for m, _, _, _ in gen_methods(mock_h, mock_D)]
+
+
+def gen_classifiers():
+    yield "LR", LogisticRegression()
+
+
+def get_dataset_names():
+    return [d for d, _ in gen_datasets(only_names=True)]
+
+
+def get_classifier_names():
+    return [c for c, _ in gen_classifiers()]
+
+
+def get_acc_names():
+    return [acc_name for acc_name, _ in gen_acc_measure()]
 
 
 @dataclass
-class EXP:
+class EXP_ss:
     code: int
     cls_name: str
     dataset_name: str
@@ -94,15 +116,15 @@ class EXP:
 
     @classmethod
     def SUCCESS(cls, *args, **kwargs):
-        return EXP(200, *args, **kwargs)
+        return EXP_ss(200, *args, **kwargs)
 
     @classmethod
     def EXISTS(cls, *args, **kwargs):
-        return EXP(300, *args, **kwargs)
+        return EXP_ss(300, *args, **kwargs)
 
     @classmethod
     def ERROR(cls, e, *args, **kwargs):
-        return EXP(400, *args, err=e, **kwargs)
+        return EXP_ss(400, *args, err=e, **kwargs)
 
     @property
     def ok(self):
@@ -127,9 +149,11 @@ def exp_protocol(args):
     for acc_name, acc_fn in gen_acc_measure():
         if is_excluded(cls_name, dataset_name, method_name, acc_name):
             continue
-        path = local_path(dataset_name, sample_size, cls_name, method_name, acc_name)
+        path = local_path(
+            dataset_full_name(dataset_name, sample_size), cls_name, method_name, acc_name, subproject=SUBPROJECT
+        )
         if os.path.exists(path):
-            results.append(EXP.EXISTS(cls_name, dataset_name, sample_size, acc_name, method_name))
+            results.append(EXP_ss.EXISTS(cls_name, dataset_name, sample_size, acc_name, method_name))
             continue
 
         try:
@@ -144,7 +168,7 @@ def exp_protocol(args):
                 estim_cts = [ct.tolist() for ct in estim_cts]
         except Exception as e:
             print_exception(e)
-            results.append(EXP.ERROR(e, cls_name, dataset_name, sample_size, acc_name, method_name))
+            results.append(EXP_ss.ERROR(e, cls_name, dataset_name, sample_size, acc_name, method_name))
             continue
 
         ae = qc.error.ae(np.array(true_accs[acc_name]), np.array(estim_accs)).tolist()
@@ -171,7 +195,7 @@ def exp_protocol(args):
         get_extra_from_method(method_df, method)
 
         results.append(
-            EXP.SUCCESS(
+            EXP_ss.SUCCESS(
                 cls_name,
                 dataset_name,
                 sample_size,
@@ -191,7 +215,7 @@ def train_cls(args):
     #
     # check if all results for current combination already exist
     # if so, skip the combination
-    if all_exist_pre_check(dataset_name, sample_size, cls_name):
+    if all_exist_pre_check(dataset_full_name(dataset_name, sample_size), cls_name, subproject=SUBPROJECT):
         return (cls_name, dataset_name, sample_size, None, None, None)
     else:
         # clone model from the original one
@@ -241,23 +265,32 @@ def experiments():
         args_list=exp_prot_args_list,
         n_jobs=qc.env["N_JOBS"],
         return_as="generator_unordered",
+        max_nbytes=None,
     )
 
+    exp_cnt, n_exp = 0, len(exp_prot_args_list) * len(get_acc_names())
     for res in results_gen:
         for r in res:
+            exp_cnt += 1
             if r.ok:
-                path = local_path(r.dataset_name, r.sample_size, r.cls_name, r.method_name, r.acc_name)
+                path = local_path(
+                    dataset_full_name(r.dataset_name, r.sample_size),
+                    r.cls_name,
+                    r.method_name,
+                    r.acc_name,
+                    subproject=SUBPROJECT,
+                )
                 r.df.to_json(path)
                 log.info(
-                    f"[{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name} on {r.acc_name} done [{timestamp(r.t_train, r.t_test_ave)}]"
+                    f"({exp_cnt}/{n_exp}) [{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name} on {r.acc_name} done [{timestamp(r.t_train, r.t_test_ave)}]"
                 )
             elif r.old:
                 log.info(
-                    f"[{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name} on {r.acc_name} exists, skipping"
+                    f"({exp_cnt}/{n_exp}) [{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name} on {r.acc_name} exists, skipping"
                 )
             elif r.error:
                 log.warning(
-                    f"[{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name}: {r.acc_name} gave error '{r.err}' - skipping"
+                    f"({exp_cnt}/{n_exp}) [{r.cls_name}@{r.dataset_name}_{r.sample_size}] {r.method_name}: {r.acc_name} gave error '{r.err}' - skipping"
                 )
 
 
