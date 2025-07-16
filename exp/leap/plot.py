0a301d99153d297bc9a682befc63298c028b9e78
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-16 14:17:21 +0200

    all plots merget into plot.py (leap)

--- exp/leap/plot.py
+++ exp/leap/plot.py
@@ -1,20 +1,36 @@
 import itertools as IT
+import math
 import os
+import pdb
+from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
 import numpy as np
+import pandas as pd
 import seaborn as sns
+from matplotlib.pyplot import close
+from matplotlib.ticker import LogLocator, MultipleLocator
 
-from exp.leap.config import PROBLEM, get_acc_names, get_classifier_names, get_dataset_names, root_dir
+import exp.leap.config as cfg
+import exp.leap.env as env
+from exp.leap.config import get_acc_names, get_classifier_names, get_dataset_names
 from exp.leap.util import load_results, rename_datasets, rename_methods
 from quacc.plot.seaborn import plot_diagonal_grid
+from quacc.plot.utils import get_binned_values, save_figure
 
 method_map = {
+    # "Naive": 'Na\\"ive',
+    "ATC-MC": "ATC",
+    "CBPE": "L-CBPE",
+    "LEAP(ACC)": "LEAP$_{\\mathrm{ACC}}$",
     "LEAP(KDEy-MLP)": "LEAP$_{\\mathrm{KDEy}}$",
-    "PHD(KDEy-MLP)": "S-LEAP$_{\\mathrm{KDEy}}$",
-    "OCE(KDEy-MLP)-SLSQP": "O-LEAP$_{\\mathrm{KDEy}}$",
-    "OCE(CC-MLP)-SLSQP": "O-LEAP$_{\\mathrm{CC}}$",
-    "OCE(oracle)-SLSQP": "O-LEAP$_{\\Phi}$",
+    "S-LEAP(KDEy-MLP)": "S-LEAP$_{\\mathrm{KDEy}}$",
+    "O-LEAP(KDEy-MLP)": "O-LEAP$_{\\mathrm{KDEy}}$",
+    "O-LEAP(CC-MLP)": "O-LEAP$_{\\mathrm{CC}}$",
+    "O-LEAP(oracle)": "O-LEAP$_{\\Phi}$",
+    "LEAP(KDEy-MLP)-SLSQP": "LEAP$_{\\mathrm{KDEy}}^{\\mathrm{SLSQP}}$",
+    "O-LEAP(KDEy-MLP)-SLSQP": "O-LEAP$_{\\mathrm{KDEy}}^{\\mathrm{SLSQP}}$",
 }
 
 dataset_map = {
@@ -24,34 +40,35 @@ dataset_map = {
     "image_seg": "image-seg",
 }
 
+acc_map = {
+    "vanilla_accuracy": "Vanilla Acc.",
+}
+
 
 def get_selection_datasets():
-    if PROBLEM == "binary":
+    if env.PROBLEM == "binary":
         return ["pageblocks.5", "yeast", "haberman", "iris.2"]
-    elif PROBLEM == "multiclass":
+    elif env.PROBLEM == "multiclass":
         return ["poker_hand", "shuttle", "page_block", "phishing"]
 
 
-def get_palette():
-    base_palette = sns.color_palette("bright", 10)
-    _dict = {
-        "DoC": 0,
-        "LEAP(KDEy-MLP)": 1,
-        "PHD(KDEy-MLP)": 2,
-        "OCE(KDEy-MLP)-SLSQP": 3,
-        "OCE(CC-MLP)-SLSQP": 4,
-        "OCE(oracle)-SLSQP": 8,
-    }
-    return {method_map.get(k, k): base_palette[v] for k, v in _dict.items()}
-
-
 def plots():
-    all_classifiers = get_classifier_names()
-    accs = get_acc_names()
-    main_methods = ["DoC", "LEAP(KDEy-MLP)", "PHD(KDEy-MLP)", "OCE(KDEy-MLP)-SLSQP"]
-    oracle_methods = ["OCE(CC-MLP)-SLSQP", "OCE(KDEy-MLP)-SLSQP", "OCE(oracle)-SLSQP"]
+    def get_palette():
+        base_palette = sns.color_palette("bright", 10)
+        _dict = {
+            "DoC": 0,
+            "LEAP(KDEy-MLP)": 1,
+            "S-LEAP(KDEy-MLP)": 2,
+            "O-LEAP(KDEy-MLP)": 3,
+            "O-LEAP(CC-MLP)": 4,
+            "O-LEAP(oracle)": 8,
+        }
+        return {method_map.get(k, k): base_palette[v] for k, v in _dict.items()}
 
-    res = load_results(filter_methods=oracle_methods + main_methods)
+    all_classifiers = get_classifier_names()
+    accs = ["vanilla_accuracy"]
+    main_methods = ["DoC", "LEAP(KDEy-MLP)", "S-LEAP(KDEy-MLP)", "O-LEAP(KDEy-MLP)"]
+    oracle_methods = ["O-LEAP(CC-MLP)", "O-LEAP(KDEy-MLP)", "O-LEAP(oracle)"]
 
     configs = [
         {
@@ -80,32 +97,27 @@ def plots():
         },
     ]
 
-    parent_dir = os.path.join(root_dir, "plots")
+    parent_dir = os.path.join(env.root_dir, "plots")
     os.makedirs(parent_dir, exist_ok=True)
 
-    for cfg, acc in IT.product(configs, accs):
-        name, classifiers, datasets, methods = cfg["name"], cfg["classifiers"], cfg["datasets"], cfg["methods"]
+    for _cfg, acc in IT.product(configs, accs):
+        name, classifiers, datasets, methods = _cfg["name"], _cfg["classifiers"], _cfg["datasets"], _cfg["methods"]
         for cls_name in classifiers:
-            df = res.loc[
-                (res["dataset"].isin(datasets))
-                & (res["method"].isin(methods))
-                & (res["acc_name"] == acc)
-                & (res["classifier"] == cls_name),
-                :,
-            ]
+            res = load_results(acc=acc, classifier=cls_name, filter_methods=methods)
+            df = res.loc[(res["dataset"].isin(datasets)) & (res["method"].isin(methods)), :]
             print(f"Plotting {name} for {cls_name} [{acc}]")
             assert len(df["method"].unique()) == len(methods), (
                 f"Error while generating {name} for {cls_name} [{acc}]: some methods missing!"
             )
-            _df, _methods = rename_methods(method_map, df, methods)
-            _df, _datasets = rename_datasets(dataset_map, df, datasets)
+            _methods, _df = rename_methods(method_map, methods, df=df)
+            _datasets, _df = rename_datasets(dataset_map, datasets, df=df)
 
             plot_diagonal_grid(
                 _df,
                 methods_order=_methods,
                 datasets_order=_datasets,
                 basedir=parent_dir,
-                filename=f"grid_{cls_name}_{PROBLEM}_{name}",
+                filename=f"grid_{cls_name}_{env.PROBLEM}_{name}",
                 n_cols=4,
                 legend_bbox_to_anchor=(0.95, 0.3),
                 palette=get_palette(),
@@ -118,52 +130,382 @@ def plots():
             )
 
 
-def sample_size_plot():
-    methods = ["DoC", "LEAP(KDEy-MLP)", "PHD(KDEy-MLP)", "OCE(KDEy-MLP)-SLSQP"]
-    classifiers = get_classifier_names()
-    accs = get_acc_names()
+def plot_sample_size():
+    from exp.leap.sample_size import get_acc_names as ss_acc_names
+    from exp.leap.sample_size import get_classifier_names as ss_classifier_names
+    from exp.leap.sample_size import get_dataset_names as ss_dataset_names
 
-    res_dir = os.path.join(root_dir, "sample_size")
-    res = load_results(base_dir=res_dir, filter_methods=methods)
+    # methods = ["DoC", "LEAP(KDEy-MLP)", "PHD(KDEy-MLP)", "OCE(KDEy-MLP)-SLSQP"]
+    methods = ["DoC", "O-LEAP(KDEy-MLP)"]
+    classifiers = ss_classifier_names()
+    datasets = ss_dataset_names()
+    accs = ss_acc_names()
+
+    res_dir = os.path.join(env.root_dir, "sample_size")
 
     for acc, cls_name in IT.product(accs, classifiers):
-        df = res.loc[(res["method"].isin(methods)) & (res["acc_name"] == acc) & (res["classifier"] == cls_name)]
-        print(f"Plotting {cls_name} [{acc}]")
+        res = load_results(base_dir=res_dir, acc=acc, classifier=cls_name, filter_methods=methods)
+        df = res.loc[res["method"].isin(methods), :]
+        print(f"Plotting {cls_name} [{acc} - {env.PROBLEM}]")
         assert len(df["method"].unique()) == len(methods), (
             f"Error while plotting {cls_name} [{acc}]: some methods missing!"
         )
-        _df, _methods = rename_methods(method_map, df, methods)
+        _methods, _df = rename_methods(method_map, methods, df=df)
+        _datasets, _df = rename_datasets(dataset_map, datasets, df=_df)
 
-        plot = sns.lineplot(
-            data=_df,
+        plot = sns.relplot(
+            _df,
             x="sample_size",
             y="acc_err",
+            col="dataset",
+            col_order=_datasets,
+            col_wrap=len(datasets),
             hue="method",
+            hue_order=_methods,
+            kind="line",
+            # sns.lineplot args
             estimator="mean",
-            errorbar=None,
+            errorbar="se",
+            err_style="bars",
+            err_kws=dict(capsize=2.0, capthick=1.0),
             linewidth=1,
         )
 
+        # set plot title
+        plot.set_titles("{col_name}")
+
         # config legend
-        plot.legend(title="")
-        sns.move_legend(plot, "lower center", bbox_to_anchor=(0.95, 0.3), ncol=1)
+        plot.legend.set_title(None)
+        sns.move_legend(
+            plot,
+            "center right",
+            # bbox_to_anchor=(1.07, 0.3),
+            ncol=1,
+        )
 
         # set axes labels
-        plot.set_xlabel("Sample size")
-        plot.set_ylabel("Accuracy Error")
+        plot.set_xlabels("Sample size")
+        plot.set_ylabels("AE")
+
+        # set log scale for x axis
+        plt.xscale("log")
 
         # plot directory
-        plot_dir = os.path.join(root_dir, "plots", "sample_size")
+        plot_dir = os.path.join(env.root_dir, "plots", "sample_size")
         os.makedirs(plot_dir, exist_ok=True)
+
         # save figure
-        exts = [".pdf", ".png"]
-        files = [os.path.join(plot_dir, f"{cls_name}_{acc}.{ext}") for ext in exts]
+        exts = ["pdf", "png"]
+        files = [os.path.join(plot_dir, f"sample_size_{cls_name}_{env.PROBLEM}.{ext}") for ext in exts]
         for f in files:
             plot.figure.savefig(f, bbox_inches="tight", dpi=300)
         plot.figure.clear()
         plt.close(plot.figure)
 
 
+def plot_times():
+    times_method_map = method_map | {
+        "LEAP(KDEy-MLP)": "LEAP$^{\\;\\!\\mathrm{sparse}}$",
+        "S-LEAP(KDEy-MLP)": "S-LEAP$^{\\;\\!\\mathrm{sparse}}$",
+        "O-LEAP(KDEy-MLP)": "O-LEAP$^{\\;\\!\\mathrm{sparse}}$",
+        "LEAP(KDEy-MLP)-SLSQP": "LEAP$^{\\;\\!\\mathrm{dense}}$",
+        "O-LEAP(KDEy-MLP)-SLSQP": "O-LEAP$^{\\;\\!\\mathrm{dense}}$",
+    }
+
+    def get_palette():
+        base_palette = sns.color_palette("Paired")
+        _dict = {
+            "ATC-MC": 0,
+            "DoC": 1,
+            "LEAP(KDEy-MLP)": 3,
+            "S-LEAP(KDEy-MLP)": 7,
+            "O-LEAP(KDEy-MLP)": 5,
+            "LEAP(KDEy-MLP)-SLSQP": 2,
+            "O-LEAP(KDEy-MLP)-SLSQP": 4,
+        }
+        return {times_method_map.get(k, k): base_palette[v] for k, v in _dict.items()}
+
+    def _get_n_classes(train_prev):
+        if isinstance(train_prev, list):
+            return len(train_prev) + 1
+        elif isinstance(train_prev, float):
+            return 2
+        else:
+            return 0
+
+    classifiers = ["LR"]
+    accs = ["vanilla_accuracy"]
+    datasets = get_dataset_names()
+    methods = [
+        "ATC-MC",
+        "DoC",
+        # "DS",
+        # "CBPE",
+        # "NN",
+        # "Q-COT",
+        "LEAP(KDEy-MLP)",
+        "S-LEAP(KDEy-MLP)",
+        "O-LEAP(KDEy-MLP)",
+        "LEAP(KDEy-MLP)-SLSQP",
+        "O-LEAP(KDEy-MLP)-SLSQP",
+    ]
+
+    parent_dir = os.path.join(env.root_dir, "plots", "times")
+    os.makedirs(parent_dir, exist_ok=True)
+
+    dfs = []
+    for acc, cls_name in IT.product(accs, classifiers):
+        df = load_results(acc=acc, classifier=cls_name, filter_methods=methods)
+        if df.empty:
+            continue
+        df = df.loc[
+            (df["dataset"].isin(datasets)) & (df["method"].isin(methods)),
+            ["t_train", "t_test_ave", "method", "dataset", "train_prev"],
+        ]
+        df["n_classes"] = df["train_prev"].map(_get_n_classes)
+        df = df.drop(columns=["train_prev"])
+        df = df.groupby(by=["n_classes", "method", "dataset"]).mean().reset_index()
+        dfs.append(df)
+
+    res = pd.concat(dfs)
+    _methods, _df = rename_methods(times_method_map, methods, df=res)
+
+    # pivot(df, parent_dir)
+    exts = ["png", "pdf"]
+    paths = [os.path.join(parent_dir, f"time_plot_{env.PROBLEM}.{ext}") for ext in exts]
+
+    plot = sns.lineplot(
+        data=_df,
+        x="n_classes",
+        y="t_test_ave",
+        hue="method",
+        hue_order=_methods,
+        errorbar="sd",
+        err_style="bars",
+        err_kws=dict(capsize=2.0, capthick=1.0),
+        palette=get_palette(),
+    )
+    plot.legend(title="")
+    plot.set_xlabel("Number of classes ($n$)")
+    plot.set_ylabel("Avg. time log (s)")
+    plot.set(yscale="log")
+    plot.xaxis.set_major_locator(MultipleLocator(2, offset=df["n_classes"].min()))
+    plot.yaxis.set_major_locator(LogLocator(10))
+    plot.yaxis.set_minor_locator(MultipleLocator(1e-2))
+    plot.minorticks_on()
+    plot.grid(which="minor")
+
+    for p in paths:
+        plot.figure.savefig(p)
+    plot.figure.clear()
+    plt.close(plot.figure)
+
+
+def plot_ctdiffs():
+    N_COLS = 4
+
+    def get_cts(df, method, cts_name):
+        return np.array(df.loc[df["method"] == method, cts_name].to_list())
+
+    def draw_heatmap(data, plot_names, **kwargs):
+        col = data["col"].to_numpy()[0]
+        row = data["row"].to_numpy()[0]
+        plot_name = plot_names[col + N_COLS * row]
+        data = data.drop(["col", "row"], axis=1).to_numpy()
+        plot = sns.heatmap(data, **kwargs)
+        plot.set_title(plot_name)
+
+    classifiers = get_classifier_names()
+    accs = get_acc_names()
+    datasets = get_dataset_names()
+    methods = ["Naive", "LEAP(KDEy-MLP)", "S-LEAP(KDEy-MLP)", "O-LEAP(KDEy-MLP)"]
+
+    parent_dir = os.path.join(env.root_dir, "plots", "ctdiffs")
+    os.makedirs(parent_dir, exist_ok=True)
+
+    for cls_name, acc, dataset in IT.product(classifiers, accs, datasets):
+        df = load_results(acc=acc, classifier=cls_name, dataset=dataset, filter_methods=methods)
+        dataset, df = rename_datasets(dataset_map, dataset, df=df)
+        methods, df = rename_methods(method_map, methods, df=df)
+
+        cnt = 0
+
+        plot_names, cbars = [], []
+        vmin, vmax = 1, 0
+        annot = False
+
+        mdfs = []
+        for m in methods:
+            true_cts = get_cts(df, m, "true_cts")
+            estim_cts = get_cts(df, m, "estim_cts")
+            _ae = np.abs(estim_cts - true_cts).mean(axis=0)
+            sqae = np.sqrt(_ae)
+
+            mdf = pd.DataFrame(data=sqae)
+            mdf["col"] = cnt % N_COLS
+            mdf["row"] = 0
+            mdfs.append(mdf)
+            plot_names.append(m)
+            cbars.append(cnt == len(methods) - 1)
+            ae_min, ae_max = np.min(sqae), np.max(sqae)
+            vmin = ae_min if ae_min < vmin else vmin
+            vmax = ae_max if ae_max > vmax else vmax
+            annot = sqae.shape[1] <= 4
+            cnt += 1
+
+        hmdf = pd.concat(mdfs, axis=0)
+        plot = sns.FacetGrid(hmdf, col="col", row="row")
+        cbar_ax = plot.fig.add_axes([0.92, 0.15, 0.02, 0.7])
+        plot = plot.map_dataframe(
+            draw_heatmap,
+            plot_names=plot_names,
+            vmin=vmin,
+            vmax=vmax,
+            cmap="rocket_r",
+            annot=annot,
+            cbar_ax=cbar_ax,
+        )
+        plot.fig.subplots_adjust(right=0.9)
+        for ax in plot.axes.flatten():
+            formatter = tkr.FuncFormatter(lambda x, p: "$\\omega_{" + f"{int(math.floor(x) + 1)}" + "}$")
+            ax.xaxis.set_major_formatter(formatter)
+            ax.yaxis.set_major_formatter(formatter)
+
+        # save figure
+        save_figure(plot=plot, basedir=parent_dir, filename=f"heatmap_{cls_name}_{dataset}_{env.PROBLEM}")
+
+
+def plot_by_shift():
+    n_bins = 20
+    classifiers = ["LR"]
+    accs = ["vanilla_accuracy"]
+    datasets = get_dataset_names()
+    methods = [
+        "Naive",
+        "ATC-MC",
+        "DoC",
+        "DS",
+        "CBPE",
+        "NN",
+        "Q-COT",
+        "LEAP(ACC)",
+        "LEAP(KDEy-MLP)",
+        "S-LEAP(KDEy-MLP)",
+        "O-LEAP(KDEy-MLP)",
+    ]
+
+    for acc, cls_name in IT.product(accs, classifiers):
+        df = load_results(acc=acc, classifier=cls_name, filter_methods=methods)
+        df.loc[:, "shifts_bin"] = get_binned_values(df, "shifts", n_bins)
+        _methods, df = rename_methods(method_map, methods, df=df)
+
+        base_dir = os.path.join(env.root_dir, "plots", "shift")
+        os.makedirs(base_dir, exist_ok=True)
+
+        # plot cumulative data for all datasets
+        plot = sns.lineplot(
+            data=df,
+            x="shifts_bin",
+            y="acc_err",
+            hue="method",
+            hue_order=_methods,
+            estimator="mean",
+            errorbar="se",
+            err_style="bars",
+            err_kws=dict(capsize=2.0, capthick=1.0),
+            linewidth=1,
+            palette=sns.color_palette("Paired")[:10] + sns.color_palette("Paired")[11:],
+        )
+        plot.set_xlabel("Amount of PPS")
+        plot.set_ylabel("AE")
+
+        sns.move_legend(plot, "center right", bbox_to_anchor=(1.2, 0.5), title=None, frameon=False)
+        save_figure(plot=plot, basedir=base_dir, filename=f"shift_{cls_name}_{env.PROBLEM}")
+        print(f"Plotted {cls_name} - {acc} - {env.PROBLEM}")
+
+
+def pseudo_label_shift_plot():
+    def get_pseudo_label_shift(df: pd.DataFrame, datasets: list[str]):
+        for d in datasets:
+            _df = df.loc[df["dataset"] == d, :]
+            true_cts = np.stack(_df["true_cts"].to_numpy(), axis=0)
+            _posteriors = true_cts.sum(axis=1)
+            _priors = true_cts.sum(axis=2)
+            pl_shift = np.abs(_priors - _posteriors).sum(axis=1)
+            df.loc[df["dataset"] == d, "pl_shift"] = pl_shift
+
+    n_bins = 20
+    classifiers = ["LR"]
+    accs = ["vanilla_accuracy"]
+    datasets = get_dataset_names()
+    methods = [
+        # "DoC",
+        "LEAP(ACC)",
+        "LEAP(KDEy-MLP)",
+        "O-LEAP(KDEy-MLP)",
+    ]
+
+    for acc, cls_name in IT.product(accs, classifiers):
+        df = load_results(acc=acc, classifier=cls_name, filter_methods=methods)
+        get_pseudo_label_shift(df, datasets)
+        df.loc[:, "pl_shift_bin"] = get_binned_values(df, "pl_shift", n_bins)
+        _methods, df = rename_methods(method_map, methods, df=df)
+
+        base_dir = os.path.join(env.root_dir, "plots", "pl_shift")
+        os.makedirs(base_dir, exist_ok=True)
+
+        # plot cumulative data for all datasets
+        plot = sns.lineplot(
+            data=df,
+            x="pl_shift_bin",
+            y="acc_err",
+            hue="method",
+            hue_order=_methods,
+            estimator="mean",
+            errorbar="se",
+            err_style="bars",
+            err_kws=dict(capsize=2.0, capthick=1.0),
+            linewidth=1,
+            # palette=sns.color_palette("Paired")[:10] + sns.color_palette("Paired")[11:],
+        )
+        plot.set_xlabel("Amount of pseudo-label shift")
+        plot.set_ylabel("AE")
+
+        sns.move_legend(plot, "center right", bbox_to_anchor=(1.2, 0.5), title=None, frameon=False)
+        save_figure(plot=plot, basedir=base_dir, filename=f"pl_shift_{cls_name}_{env.PROBLEM}")
+        print(f"Plotted {cls_name} - {acc} - {env.PROBLEM}")
+
+
 if __name__ == "__main__":
-    # plots()
-    sample_size_plot()
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-p", "--problem", action="store", default="binary", help="Select the problem you want to generate plots for"
+    )
+    parser.add_argument("--main", action="store_true", help="Plots for main experiments")
+    parser.add_argument("--ss", action="store_true", help="Plots for sample_size experiments")
+    parser.add_argument("--times", action="store_true", help="Plots for times obtained in main experiments")
+    parser.add_argument(
+        "--ctdiff", action="store_true", help="Plots for contingency table diffs obtained in main experiments"
+    )
+    parser.add_argument("--shift", action="store_true", help="Plots accuracy error by amount of PPS")
+    parser.add_argument("--pseudo", action="store_true", help="Plots accuracy error by amount of pseudo-label shift")
+    args = parser.parse_args()
+
+    if args.problem not in env._valid_problems:
+        raise ValueError(f"Invalid problem {args.problem}: valid problems are {env._valid_problems}")
+    env.PROBLEM = args.problem
+
+    if args.main:
+        plots()
+    elif args.ss:
+        plot_sample_size()
+    elif args.times:
+        plot_times()
+    elif args.ctdiff:
+        plot_ctdiffs()
+    elif args.shift:
+        plot_by_shift()
+    elif args.pseudo:
+        pseudo_label_shift_plot()
+    else:
+        parser.print_help()
