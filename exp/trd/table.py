78c07b8ab3fddc3efa84cfddfff0d94f4fc75921
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-16 14:18:23 +0200

    improved table (trd)

--- exp/trd/table.py
+++ exp/trd/table.py
@@ -1,9 +1,10 @@
 import itertools as IT
 import os
+from hmac import new
 
 import pandas as pd
 
-from exp.trd.config import PROBLEM, get_acc_names, root_dir
+from exp.trd.config import PROBLEM, get_acc_names, get_dataset_names, root_dir
 from exp.trd.model_selection import model_selection
 from exp.trd.util import decorate_datasets, load_results, rename_datasets, rename_methods
 from quacc.table import Format, Table
@@ -25,15 +26,10 @@ dataset_map = {
 
 
 def tables():
-    res = load_results()
-    res = model_selection(res, oracle=False, only_default=True)
+    # res = load_results()
+    # res = model_selection(res, oracle=False, only_default=True)
 
-    accs = get_acc_names()
-    methods = res["method"].unique().tolist()
-    datasets = res["dataset"].unique().tolist()
-
-    def gen_table(df: pd.DataFrame, name, datasets, methods):
-        tbl = Table(name=name, benchmarks=datasets, methods=methods)
+    def add_to_table(tbl: Table, df: pd.DataFrame, dataset, methods):
         tbl.format = Format(
             lower_is_better=False,
             mean_prec=3,
@@ -46,23 +42,40 @@ def tables():
             color_mode="local",
             simple_stat=True,
         )
-        for dataset, method in IT.product(datasets, methods):
-            values = df.loc[(df["dataset"] == dataset) & (df["method"] == method), "true_accs"].to_numpy()
+        for method in methods:
+            values = df.loc[df["method"] == method, "true_accs"].to_numpy()
             for v in values:
                 tbl.add(dataset, method, v)
         return tbl
 
     tbls = []
+    accs = get_acc_names()
+    datasets = get_dataset_names()
     for acc in accs:
-        _df = res.loc[res["acc_name"] == acc, :]
         name = f"{PROBLEM}_{acc}"
-        _df, _datasets = rename_datasets(dataset_map, _df, datasets)
-        _df, _datasets = decorate_datasets(_df, _datasets)
-        _df, _methods = rename_methods(method_map, _df, methods)
-        tbls.append(gen_table(_df, name, _datasets, _methods))
+        tbl = Table(name=name)
+        for dataset in datasets:
+            _df = load_results(acc_name=acc, dataset=dataset)
+            _df = model_selection(_df, oracle=False, only_default=True)
+            print(f"{dataset} results loaded")
+            # _df = res.loc[res["acc_name"] == acc, :]
+            methods = _df["method"].unique().tolist()
+            _df, _dataset = rename_datasets(dataset_map, _df, dataset)
+            _df, _dataset = decorate_datasets(_df, _dataset)
+            _df, _methods = rename_methods(method_map, _df, methods)
+            tbl = add_to_table(tbl, _df, _dataset, _methods)
+
+        tbls.append(tbl)
+        print(f"table {name} genned")
 
     pdf_path = os.path.join(root_dir, "tables", f"{PROBLEM}.pdf")
-    Table.LatexPDF(pdf_path, tables=tbls, landscape=False)
+    new_commands = [
+        "\\newcommand{\\leapacc}{LEAP$_\\mathrm{ACC}$}",
+        "\\newcommand{\\leapplus}{LEAP$_\\mathrm{KDEy}$}",
+        "\\newcommand{\\leapppskde}{S-LEAP$_\\mathrm{KDEy}$}",
+        "\\newcommand{\\oleapkde}{O-LEAP$_\\mathrm{KDEy}$}",
+    ]
+    Table.LatexPDF(pdf_path, tables=tbls, landscape=False, new_commands=new_commands)
 
 
 if __name__ == "__main__":
