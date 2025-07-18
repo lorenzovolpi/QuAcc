77efed644a1aefd152d42ff2361d543dbd6d1783
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-18 22:08:23 +0200

    environment refactored

--- exp/leap/env.py
+++ exp/leap/env.py
@@ -4,6 +4,16 @@ import quapy as qp
 
 import quacc as qc
 
+qc.env["OUT_DIR"] = os.getenv("EXP_OUT_DIR", "./output")
+qc.env["QUACC_DATA"] = os.getenv("QUACC_DATA", qc.env["QUACC_DATA"])
+qc.env["QUAPY_DATA"] = os.getenv("QUACC_QUAPY_DATA", qc.env["QUAPY_DATA"])
+qc.env["SKLEARN_DATA"] = os.getenv("QUACC_SKLEARN_DATA", qc.env["SKLEARN_DATA"])
+qc.env["N_JOBS"] = int(os.getenv("QUACC_N_JOBS", qc.env["N_JOBS"]))
+_force_njobs = os.getenv("QUACC_FORCE_NJOBS", None)
+if _force_njobs is not None:
+    qc.env["FORCE_NJOBS"] = int(_force_njobs) > 0
+
+
 PROJECT = "leap"
 root_dir = os.path.join(qc.env["OUT_DIR"], PROJECT)
 NUM_TEST = 1000
--- quacc/environment.py
+++ quacc/environment.py
@@ -1,10 +1,15 @@
 import os
+from glob import glob
+
+from dotenv import load_dotenv
+
+for _env in glob("./*.env"):
+    load_dotenv(_env)
 
 env = {
-    "OUT_DIR": os.getenv("QUACC_OUT_DIR", "."),
-    "QUACC_DATA": os.getenv("QUACC_DATA", os.path.expanduser("~/quacc_data")),
-    "QUAPY_DATA": os.getenv("QUACC_QUAPY_DATA", os.path.expanduser("~/quapy_data")),
-    "SKLEARN_DATA": os.getenv("QUACC_SKLEARN_DATA", os.path.expanduser("~/scikit_learn_data")),
-    "N_JOBS": int(os.getenv("QUACC_N_JOBS", -2)),
-    "FORCE_NJOBS": int(os.getenv("QUACC_FORCE_NJOBS", 0)) > 0,
+    "QUACC_DATA": os.path.expanduser("~/quacc_data"),
+    "QUAPY_DATA": os.path.expanduser("~/quapy_data"),
+    "SKLEARN_DATA": os.path.expanduser("~/scikit_learn_data"),
+    "N_JOBS": -2,
+    "FORCE_NJOBS": True,
 }
