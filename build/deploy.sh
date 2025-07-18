d73f53dffa875e34d3396e819cce1a572484628e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-18 22:08:13 +0200

    deploy.sh adapted to calibration refactoring

--- build/deploy.sh
+++ build/deploy.sh
@@ -21,7 +21,7 @@ if [[ $SERVER == "dgx" ]]; then
 
   DEST="$HOST:$WORKDIR"
 
-  rsync -ai calibration quacc playground exp $DEST
+  rsync -ai quacc playground exp $DEST
   rsync -i README.md build/run.sh build/dgx.env build/filter_out.py pyproject.toml $DEST
 elif [[ $SERVER == "bcuda" || $SERVER == "barracuda" ]]; then
   HOST="volpi@barracuda.isti.cnr.it"
@@ -29,7 +29,7 @@ elif [[ $SERVER == "bcuda" || $SERVER == "barracuda" ]]; then
 
   DEST="$HOST:$WORKDIR"
 
-  rsync -ai calibration quacc playground exp $DEST
+  rsync -ai quacc playground exp $DEST
   rsync -i README.md build/run.sh build/bcuda.env build/filter_out.py pyproject.toml $DEST
   rsync -ai qcdash $DEST
   rsync -i build/dash.sh build/dash.env $DEST
@@ -39,6 +39,6 @@ elif [[ $SERVER == "trinity"  || $SERVER == "nity" ]]; then
 
   DEST="$HOST:$WORKDIR"
 
-  rsync -ai calibration quacc playground exp $DEST
+  rsync -ai quacc playground exp $DEST
   rsync -i README.md build/run.sh build/nity.env build/filter_out.py pyproject.toml $DEST
 fi
