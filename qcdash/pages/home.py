dce97f00339e2e4e2f20a939da0bb3572c6a76cd
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-18 18:39:35 +0200

    qcdash removed

--- qcdash/pages/home.py
+++
@@ -1,8 +0,0 @@
-from dash import html, register_page
-
-register_page(__name__, name="Home", top_nav=True, path="/")
-
-
-def layout():
-    layout = html.Div([html.H1(["Home Page"])])
-    return layout
